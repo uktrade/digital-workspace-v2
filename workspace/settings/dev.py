5145635f85265401f46cf818d90e237b1abc1ee4
Christian Sutter <git@doublesignal.com>
2019-12-10 16:45:56 +0000

    Initial generated Wagtail project

---
+++ workspace/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^yz=fa@$1in-6)+@(^yuwfogc=52x*r95o06rvmgy!dre83wv4'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
