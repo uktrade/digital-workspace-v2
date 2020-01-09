a0bf56c7ff906552c986ef3f6d5ae6a1d922e01a
Christian Sutter <git@doublesignal.com>
2020-01-09 10:06:49 +0000

    Set up basic logging configuration

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,30 @@ if env.str("SENTRY_DSN", None):
         integrations=[DjangoIntegration()]
     )
 
+
+# Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['console']
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] [%(name)s] %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    }
+}
+
+
 # This application will always be run behind a PaaS router (or locally),
 # so all hosts can be whitelisted
 ALLOWED_HOSTS = ["*"]
