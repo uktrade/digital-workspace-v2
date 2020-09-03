50089a889c1edd6f106a0a8c34dc20ca3bd7f068
Ross Miller <github@decodedcollective.com>
2020-09-03 14:38:32 +0100

    Added child pages to transition

--- Transition/models.py
+++ Transition/models.py
@@ -1,14 +1,23 @@
 from content.models import ContentPage
 
 
-class TransitionHome(ContentPage):
-    is_creatable = False
-
-    subpage_types = ["transition.Transition", ]
-
-
 class Transition(ContentPage):
     is_creatable = True
 
     parent_page_types = ['transition.TransitionHome']
     subpage_types = ["transition.Transition", ]
+
+
+class TransitionHome(ContentPage):
+    is_creatable = False
+
+    subpage_types = ["transition.Transition", ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        children = Transition.objects.all().order_by("title")
+
+        context["children"] = children
+
+        return context
