dfbef7c29f75daedfe80317d1ea0b673e33690dd
Ross Miller <github@decodedcollective.com>
2020-09-15 15:06:25 +0100

    Attempt to get S3 static files to work

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,6 +8,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_DEFAULT_ACL = None
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
