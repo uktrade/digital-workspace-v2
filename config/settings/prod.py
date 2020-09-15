32c387d5e7ed2bd314a1eb10f6481f698b5ebf7d
Ross Miller <github@decodedcollective.com>
2020-09-15 15:59:34 +0100

    Remove static files from S3

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ MIDDLEWARE += [
 ]
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_DEFAULT_ACL = None
 AWS_S3_SIGNATURE_VERSION = 's3v4'
