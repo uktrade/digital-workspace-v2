9e26d06a606ce9f45396dbc544042d70027daadf
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-02-19 14:29:23 +0000

    WCM-236 Add PDF document uploads for country factsheets
    Made use of Wagtail collections for the document handling.

--- country_fact_sheet/models.py
+++ country_fact_sheet/models.py
@@ -1,129 +1,22 @@
 import logging
 
-from bs4 import BeautifulSoup
-from django import forms
 from django.contrib.auth import get_user_model
 from django.db import models
-from wagtail.admin.edit_handlers import ObjectList, TabbedInterface
-from wagtail.admin.forms import WagtailAdminPageForm
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    ObjectList,
+    TabbedInterface,
+)
+from wagtail.documents.models import Document
 
-from content.models import BasePage, ContentPage
-from country_fact_sheet.tasks import process_fact_sheets
+from content.models import ContentPage
 
 
 UserModel = get_user_model()
 
 
 logger = logging.getLogger(__name__)
-
-
-class CountryFactSheetUpload(models.Model):
-    UNPROCESSED = "unprocessed"
-    PROCESSING = "processing"
-    PROCESSED = "processed"
-    PROCESSED_WITH_ERROR = "processed_error"
-    ANTIVIRUS = "antivirus"
-    PROCESSED_WITH_VIRUS = "Found virus"
-
-    STATUS_CHOICES = [
-        (UNPROCESSED, "Unprocessed"),
-        (ANTIVIRUS, "Checking for viruses"),
-        (PROCESSED_WITH_ERROR, "Processed. Page not created, error(s) found."),
-        (PROCESSED_WITH_VIRUS, "Processed. Found virus."),
-        (PROCESSING, "Processing"),
-        (PROCESSED, "Processed and page created"),
-    ]
-
-    uploaded_on = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, related_name="users"
-    )
-    country_name = models.CharField(max_length=255)
-    s3_document_file = models.FileField(
-        max_length=1000,
-        null=True,
-        blank=True,
-    )
-    status = models.CharField(
-        max_length=100,
-        choices=STATUS_CHOICES,
-        default=UNPROCESSED,
-    )
-    user_error_message = models.TextField(
-        null=True,
-        blank=True,
-    )
-    error_message = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True,
-    )
-
-
-class CountryFactSheet(BasePage):
-    subpage_types = []  # Should not be able to create children
-    is_creatable = False
-
-    fact_sheet_content = models.TextField(
-        blank=True,
-        null=True,
-    )
-
-    fact_sheet_content_no_html = models.TextField(
-        blank=True,
-        null=True,
-    )
-
-    def save(self, *args, **kwargs):
-        self.fact_sheet_content_no_html = BeautifulSoup(
-            str(self.fact_sheet_content), "html.parser"
-        ).text
-
-        return super().save(*args, **kwargs)
-
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request, *args, **kwargs)
-        context["fact_sheet_content"] = self.fact_sheet_content
-
-        return context
-
-
-class CountryFactSheetHomeForm(WagtailAdminPageForm):
-    country_fact_sheets = forms.FileField(
-        widget=forms.ClearableFileInput(attrs={"multiple": True}),
-        required=False,
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def clean(self):
-        cleaned_data = super().clean()
-
-        return cleaned_data
-
-    def save(self, commit=True):
-        page = super().save(commit=False)
-
-        country_fact_sheets = self.cleaned_data["country_fact_sheets"]
-
-        if country_fact_sheets:
-            files = self.files.getlist("country_fact_sheets")
-            for file in files:
-                logger.info(f"Processing '{file.name}' country file")
-                # When using a model form, you must use the
-                # name attribute of the file rather than
-                # passing the request file var directly as this is the
-                # required when using the chunk uploader project
-                CountryFactSheetUpload.objects.create(
-                    s3_document_file=file.name,
-                    uploaded_by=self.initial["user"],
-                )
-            process_fact_sheets.delay()
-
-        if commit:
-            page.save()
-        return page
 
 
 class CustomTabbedInterface(TabbedInterface):
@@ -149,13 +42,63 @@ class CustomTabbedInterface(TabbedInterface):
 
 
 class CountryFactSheetHome(ContentPage):
-    subpage_types = [
-        "country_fact_sheet.CountryFactSheet",
+    country_factsheets_help_text = (
+        "Uploaded PDF documents in the selected collection will be displayed"
+        " as a list of country factsheets on the page."
+    )
+
+    hmtc_region_factsheets_collection = models.ForeignKey(
+        "wagtailcore.Collection",
+        models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+        verbose_name="HMTC region factsheets collection",
+    )
+    group_factsheets_collection = models.ForeignKey(
+        "wagtailcore.Collection",
+        models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="+",
+        verbose_name="Group factsheets collection",
+    )
+
+    hmtc_region_factsheets_heading = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        verbose_name="HMTC region factsheets heading",
+    )
+    group_factsheets_heading = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        verbose_name="Group factsheets heading",
+    )
+
+    content_panels = ContentPage.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("hmtc_region_factsheets_heading"),
+                FieldPanel("hmtc_region_factsheets_collection"),
+            ],
+            heading="HMTC region factsheets",
+            help_text=country_factsheets_help_text,
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("group_factsheets_heading"),
+                FieldPanel("group_factsheets_collection"),
+            ],
+            heading="Group factsheets",
+            help_text=country_factsheets_help_text,
+        ),
     ]
 
     edit_handler = CustomTabbedInterface(
         [
-            ObjectList(ContentPage.content_panels, heading="Content"),
+            ObjectList(content_panels, heading="Content"),
             ObjectList(ContentPage.promote_panels, heading="Promote"),
             ObjectList(
                 ContentPage.settings_panels, heading="Settings", classname="settings"
@@ -163,16 +106,25 @@ class CountryFactSheetHome(ContentPage):
         ]
     )
 
-    def form_valid(self, form, *args, **kwargs):
-        return super().form_valid(form, *args, **kwargs)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def form_valid(self, form, *args, **kwargs):
+        return super().form_valid(form, *args, **kwargs)
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
 
-        # TODO - add listing of country fact sheets
+        if context["page"].hmtc_region_factsheets_collection:
+            context["hmtc_region_factsheets"] = Document.objects.filter(
+                collection=context["page"].hmtc_region_factsheets_collection
+            )
+
+        if context["page"].group_factsheets_collection:
+            context["group_factsheets"] = Document.objects.filter(
+                collection=context["page"].group_factsheets_collection
+            )
+
         return context
 
     def get_form_class(self):
@@ -180,5 +132,3 @@ class CountryFactSheetHome(ContentPage):
 
     def on_request_bound(self):
         return super().on_request_bound()
-
-    base_form_class = CountryFactSheetHomeForm
