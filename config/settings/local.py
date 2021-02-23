9ff1fd6661189b84e024246b036cd2e9fa23aff0
Sam Dudley <dudley.co.uk@gmail.com>
2021-02-23 11:17:19 +0000

    WCM-236 Add PDF document uploads for country factsheets (#6)
    Made use of Wagtail collections for the document handling.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,13 @@
 from .base import *  # noqa
 
 CAN_ELEVATE_SSO_USER_PERMISSIONS = True
+
+# Set to True if you need to upload documents and you are not running
+# the ClamAV service locally.
+SKIP_CLAM_AV_FILE_UPLOAD = False
+
+if SKIP_CLAM_AV_FILE_UPLOAD:
+    FILE_UPLOAD_HANDLERS = (
+        # "file_upload_handler.clam_av.ClamAVFileUploadHandler",
+        "file_upload_handler.s3.S3FileUploadHandler",
+    )  # Order is important
