5443cb87e8ae82f1f50bb6e6a1af2985fa7f48e7
Ross Miller <github@decodedcollective.com>
2021-03-30 16:35:25 +0100

    Add Elastic APM

--- config/settings/env.py
+++ config/settings/env.py
@@ -18,6 +18,17 @@ SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
+INSTALLED_APPS += [  # noqa F405
+    "elasticapm.contrib.django",
+]
+
+ELASTIC_APM = {
+    "SERVICE_NAME": "Digital Workspace",
+    "SECRET_TOKEN": env("ELASTIC_APM_SECRET_TOKEN"),  # noqa F405
+    "SERVER_URL": env("ELASTIC_APM_SERVER_URL"),  # noqa F405
+    "ENVIRONMENT": env("APP_ENV"),  # noqa F405
+    "SERVER_TIMEOUT": env("ELASTIC_APM_SERVER_TIMEOUT", default="20s"),  # noqa F405
+}
 
 LOGGING = {
     "version": 1,
