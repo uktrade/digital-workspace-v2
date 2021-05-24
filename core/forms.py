97e945805e2a8917c206f747822cb9dee7c603dd
Ross Miller <github@decodedcollective.com>
2021-05-24 12:57:40 +0100

    Swap text inputs for text areas

--- core/forms.py
+++ core/forms.py
@@ -4,15 +4,15 @@ from django import forms
 class PageProblemFoundForm(forms.Form):
     trying_to = forms.CharField(
         label="What were you trying to do?",
-        max_length=300,
-        widget=forms.TextInput(
-            attrs={"class": "govuk-input", }
+        max_length=500,
+        widget=forms.Textarea(
+            attrs={"class": "govuk-textarea", }
         )
     )
     what_went_wrong = forms.CharField(
         label='What went wrong?',
-        max_length=300,
-        widget=forms.TextInput(
-            attrs={"class": "govuk-input", }
+        max_length=500,
+        widget=forms.Textarea(
+            attrs={"class": "govuk-textarea", }
         )
     )
