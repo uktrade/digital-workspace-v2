f5efbc085f5770b1c4de874c89065792beeb8e35
Ross Miller <github@decodedcollective.com>
2021-05-25 10:08:41 +0100

    WIP

--- config/urls.py
+++ config/urls.py
@@ -42,3 +42,8 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    handler404 = ""
+    handler500 = ""
+    handler403 = ""
+    handler400 = ""
---
+++ core/management/commands/create_http_error_code_pages.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from wagtail.core.models import Page
+
+from content.models import ContentPage
+
+
+class Command(BaseCommand):
+    help = "Create 404, 500, 403 and 400 pages"
+
+    def handle(self, *args, **options):
+        home_page = Page.objects.filter(slug="home").first()
+
+        response_codes = 404, 500, 403, 400
+
+        for response_code in response_codes:
+            page = ContentPage(
+                title=f"{response_code} Error",
+                slug=f"{response_code}-page",
+                live=True,
+                first_published_at=datetime.now(),
+                show_in_menus=False,
+                depth=1,
+            )
+
+            home_page.add_child(instance=page)
+            home_page.save()
+            page.save_revision().publish()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully created page for response code '{response_code}'"
+                )
+            )
