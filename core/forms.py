772331606f4f01c477b42def27c9be83fc1b1e66
Ross Miller <github@decodedcollective.com>
2021-05-26 11:48:19 +0100

    WCM-272 Put back anything wrong link

---
+++ core/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+
+class PageProblemFoundForm(forms.Form):
+    trying_to = forms.CharField(
+        label="What were you trying to do?",
+        max_length=500,
+        widget=forms.Textarea(
+            attrs={
+                "class": "govuk-textarea",
+            }
+        ),
+    )
+    what_went_wrong = forms.CharField(
+        label="What went wrong?",
+        max_length=500,
+        widget=forms.Textarea(
+            attrs={
+                "class": "govuk-textarea",
+            }
+        ),
+    )
