8e07653533a332356710b19b749999539383d2f6
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-07-07 09:22:18 +0100

    PFM-92 Add new sub-team to team

--- peoplefinder/forms/team.py
+++ peoplefinder/forms/team.py
@@ -2,7 +2,7 @@ from django import forms
 from django.utils.text import slugify
 
 from peoplefinder.models import Team
-from peoplefinder.services.team import TeamService
+from peoplefinder.services.team import TeamService, TeamServiceError
 
 
 class TeamForm(forms.ModelForm):
@@ -22,24 +22,48 @@ class TeamForm(forms.ModelForm):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        team_service = TeamService()
+        self.team_service = TeamService()
 
-        self.initial.update(
-            parent_team=team_service.get_immediate_parent_team(self.instance)
-        )
+        # creating a new team
+        # `_state.adding` is safer than pk/id check as that doesn't work for uuids with
+        # a default value
+        # https://docs.djangoproject.com/en/3.2/ref/models/instances/#other-attributes
+        self.creating = self.instance._state.adding
+        # editing an existing team
+        self.editing = not self.creating
+
+        if self.editing:
+            self.initial.update(
+                parent_team=self.team_service.get_immediate_parent_team(self.instance)
+            )
+
+        self.is_root_team = self.team_service.get_root_team() == self.instance
 
         # parent_team is not required if we are editing the root team
-        if self.instance and team_service.get_root_team() == self.instance:
+        if self.is_root_team:
             self.fields["parent_team"].required = False
 
     def clean(self):
         cleaned_data = super().clean()
 
-        if "parent_team" in self.changed_data:
+        self.new_parent_team = None
+
+        try:
+            self.new_parent_team = Team.objects.get(pk=cleaned_data["parent_team"])
+        except Team.DoesNotExist:
+            self.add_error("parent_team", "Invalid parent team")
+
+        if (
+            self.editing
+            and ("parent_team" in self.changed_data)
+            and self.new_parent_team
+        ):
             try:
-                self.new_parent_team = Team.objects.get(pk=cleaned_data["parent_team"])
-            except Team.DoesNotExist:
-                self.add_error("parent_team", "Invalid parent_team value")
+                self.team_service.validate_team_parent_update(
+                    self.instance, self.new_parent_team
+                )
+            except TeamServiceError as e:
+                self.add_error("parent_team", str(e))
 
         return cleaned_data
 
@@ -49,11 +73,10 @@ class TeamForm(forms.ModelForm):
 
         super().save(commit=commit)
 
-        team_service = TeamService()
-
-        if "parent_team" in self.changed_data:
-            # only non-root teams can have their parent team updated
-            if self.instance != team_service.get_root_team():
-                team_service.update_team_parent(self.instance, self.new_parent_team)
+        if self.creating:
+            self.team_service.add_team(self.instance, self.new_parent_team)
+        # editing and...
+        elif "parent_team" in self.changed_data:
+            self.team_service.update_team_parent(self.instance, self.new_parent_team)
 
         return self.instance
