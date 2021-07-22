7c4e53be5bff671b4f5fed9ee4cbbac573a08ee6
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2021-07-22 14:35:59 +0100

    WCM-377 Add ability to add child pages to about us pages

--- about_us/models.py
+++ about_us/models.py
@@ -4,8 +4,18 @@ from working_at_dit.models import PageWithTopics
 class AboutUs(PageWithTopics):
     is_creatable = True
 
-    parent_page_types = ["about_us.AboutUsHome"]
+    parent_page_types = ["about_us.AboutUsHome", "about_us.AboutUs"]
     subpage_types = ["about_us.AboutUs"]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        page = context["page"]
+        context["children"] = (
+            AboutUs.objects.live().public().child_of(page).order_by("title")
+        )
+
+        return context
 
 
 class AboutUsHome(PageWithTopics):
@@ -16,6 +26,9 @@ class AboutUsHome(PageWithTopics):
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
 
-        context["children"] = AboutUs.objects.live().public().order_by("title")
+        page = context["page"]
+        context["children"] = (
+            AboutUs.objects.live().public().child_of(page).order_by("title")
+        )
 
         return context
