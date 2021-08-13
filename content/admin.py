46339cddfadcd28fdae312ea16c3a4ef93532f2a
Ross Miller <github@decodedcollective.com>
2021-08-13 16:50:05 +0100

    WIP

--- content/admin.py
+++ content/admin.py
@@ -7,9 +7,6 @@ class DocumentAdmin(SimpleHistoryAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_change_permission(self, request, obj=None):
-        return request.user.is_superuser
-
     def has_delete_permission(self, request, obj=None):
         return request.user.is_superuser
 
