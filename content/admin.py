9427fc40f890cf193fbb1ff8047679ed891c3fb7
Ross Miller <github@decodedcollective.com>
2021-08-13 16:25:05 +0100

    Lock down admin site for non super users

--- content/admin.py
+++ content/admin.py
@@ -4,6 +4,23 @@ from wagtail.documents.models import Document
 
 
 class DocumentAdmin(SimpleHistoryAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    # make everything read only for non-super users
+    def get_readonly_fields(self, request, obj=None):
+        if obj and not request.user.is_superuser:
+            self.readonly_fields = [
+                field.name for field in obj.__class__._meta.fields
+            ]
+        return self.readonly_fields
+
     list_display = ["title", "created_at", "uploaded_by_user"]
 
     search_fields = [
--- user/admin.py
+++ user/admin.py
@@ -8,8 +8,22 @@ User = get_user_model()
 
 
 class SSOUserAdmin(UserAdmin):
-    def has_add_permission(self, request, obj=None):
+    def has_add_permission(self, request):
         return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    # make everything read only for non-super users
+    def get_readonly_fields(self, request, obj=None):
+        if obj and not request.user.is_superuser:
+            self.readonly_fields = [
+                field.name for field in obj.__class__._meta.fields
+            ]
+        return self.readonly_fields
 
     exclude = ("password",)
     fieldsets = (
