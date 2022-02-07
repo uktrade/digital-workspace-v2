d0daa19a976b866cc51cd27212e9a6611141d819
Ross Miller <rossmiller@Rosss-MacBook-Pro.local>
2022-02-07 11:46:22 +0000

    Add people finder permissions and groups

--- peoplefinder/models.py
+++ peoplefinder/models.py
@@ -203,6 +203,12 @@ class Person(models.Model):
             ),
         ]
 
+    permissions = (
+        ("edit_profile", "Edit profile"),
+        ("delete_profile", "Delete profile"),
+        ("edit_sso_id", "Edit SSO ID"),
+    )
+
     user = models.OneToOneField(
         "user.User", models.CASCADE, primary_key=True, related_name="profile"
     )
@@ -431,6 +437,12 @@ You can update this description, by [updating your team information](https://wor
 
 
 class Team(models.Model):
+    permissions = (
+        ("edit_team", "Edit team"),
+        ("add_subteam", "Add sub-team"),
+        ("delete_team", "Delete team"),
+    )
+
     people = models.ManyToManyField(
         "Person", through="TeamMember", related_name="teams"
     )
