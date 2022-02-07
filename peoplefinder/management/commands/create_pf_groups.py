9a69f60021af7db3fa5f730cdad3c8caf4198490
Ross Miller <rossmiller@Rosss-MacBook-Pro.local>
2022-02-07 11:46:22 +0000

    Add people finder permissions and groups

---
+++ peoplefinder/management/commands/create_pf_groups.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
+
+
+PERSON_EDITOR_PERMISSION_TYPES = [
+    "edit_profile",
+    "delete_profile",
+    "edit_sso_id",
+]
+
+TEAM_EDITOR_PERMISSION_TYPES = [
+    "edit_team",
+    "add_subteam",
+    "delete_team",
+]
+
+
+class Command(BaseCommand):
+    help = "Create page permissions"
+
+    def handle(self, *args, **options):
+        person_editors, _ = Group.objects.get_or_create(
+            name="Person Editors",
+        )
+
+        person_editor_permissions = Permission.objects.filter(
+            codename__in=PERSON_EDITOR_PERMISSION_TYPES
+        )
+        person_editors.permissions.add(*person_editor_permissions)
+        person_editors.save()
+
+        team_editors, _ = Group.objects.get_or_create(
+            name="Team Editors",
+        )
+
+        team_editor_permissions = Permission.objects.filter(
+            codename__in=TEAM_EDITOR_PERMISSION_TYPES
+        )
+        team_editors.permissions.add(*team_editor_permissions)
+        team_editors.save()
