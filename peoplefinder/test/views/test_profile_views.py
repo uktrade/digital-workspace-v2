d750d51230cc38daff2a2964d2b537159d98fe1a
Luisella Strona <luisella21@hotmail.com>
2022-02-08 08:49:55 +0000

    Added test for edit team permission.

--- peoplefinder/test/views/test_profile_views.py
+++ peoplefinder/test/views/test_profile_views.py
@@ -1,5 +1,6 @@
 import pytest
 
+from dataclasses import dataclass
 from django.contrib.auth.models import (
     Group,
     Permission,
@@ -11,26 +12,29 @@ from peoplefinder.models import Person, Team
 from peoplefinder.services.person import PersonService
 from peoplefinder.test.factories import TeamFactory
 
+from user.models import User
 from user.test.factories import UserFactory
 
 
 @dataclass
 class State:
     client: Client
-    user: User,
-    team: Team,
+    user: User
+    team: Team
     person: Person
 
 
 @pytest.fixture()
 def state(db):
-    team = TeamFactory()
+    team = Team.objects.all().first()
+    if team == None:
+        team = TeamFactory()
     user = UserFactory()
+    user.is_using_peoplefinder_v2 = True
+    user.save()
     person = PersonService().create_user_profile(user)
-
     client = Client()
     client.force_login(user)
-
     return State(client=client, person=person,  team=team, user=user)
 
 
@@ -56,5 +60,21 @@ def test_edit_profile_permission(state):
 
 
 def test_edit_team_permission(state):
-    # TODO implement
-    pass
+    edit_url = reverse(
+        "team-edit",
+        kwargs={
+            'slug': state.team.slug,
+        }
+    )
+
+    response = state.client.get(edit_url)
+    assert response.status_code == 403
+
+    edit_team_perm = Permission.objects.get(
+        codename='change_team'
+    )
+    state.user.user_permissions.add(edit_team_perm)
+    state.user.save()
+
+    response = state.client.get(edit_url)
+    assert response.status_code == 200
