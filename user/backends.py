13a7be07e2e3c0c41f788a4225f68f0302133f22
Ross Miller <rossmiller@Rosss-MacBook-Pro.local>
2022-02-18 11:01:10 +0000

    Removed unused imports

--- user/backends.py
+++ user/backends.py
@@ -5,8 +5,6 @@ from authbroker_client.utils import (
     has_valid_token,
 )
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
-# from django.contrib.auth.models import Permission
 
 from peoplefinder.services.person import PersonService
 
