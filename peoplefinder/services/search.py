b776fda50f86c23ff3ac9a12509bd9a534bab4e3
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2022-04-04 15:31:01 +0100

    PFM-239 Improve peoplefinder search (#208)

--- peoplefinder/services/search.py
+++ peoplefinder/services/search.py
@@ -1,4 +1,5 @@
 from wagtail.search.backends import get_search_backend
+from wagtail.search.utils import parse_query_string
 
 from peoplefinder.forms.search import PEOPLE_FILTER, TEAMS_FILTER
 from peoplefinder.models import Person, Team
@@ -10,16 +11,18 @@ SearchResult = tuple[list[Team], list[Person]]
 def search(query: str, filters: list[str]) -> SearchResult:
     s = get_search_backend()
 
+    _, query = parse_query_string(query, operator="and")
+
     team_results = []
     person_results = []
 
     if TEAMS_FILTER in filters:
-        team_results = s.search(query, Team)
+        team_results = s.search(query, Team)[:50]
 
     if PEOPLE_FILTER in filters:
         person_results = s.search(
             query,
             Person.objects.prefetch_related("key_skills", "additional_roles", "teams"),
-        )
+        )[:50]
 
     return list(team_results), list(person_results)
