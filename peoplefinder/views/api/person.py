cb3dab4d0ab91e8d54152d582e2416b9c89dc93b
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2022-05-13 09:15:00 +0100

    PFM-244 Update person API for data workspace (#233)

--- peoplefinder/views/api/person.py
+++ peoplefinder/views/api/person.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.contrib.postgres.aggregates import ArrayAgg, StringAgg
+from django.db.models import Q, Value
+from django.db.models.expressions import Case, When
+from django.db.models.functions import Concat
 from django.utils.decorators import decorator_from_middleware
 from django_hawk.middleware import HawkResponseMiddleware
 from django_hawk_drf.authentication import HawkAuthentication
@@ -26,105 +29,22 @@ class TeamMemberSerialiser(serializers.ModelSerializer):
 
 
 class PersonSerializer(serializers.ModelSerializer):
-    buildings = serializers.CharField(source="building_codes")
-    works_monday = serializers.SerializerMethodField()
-    works_tuesday = serializers.SerializerMethodField()
-    works_wednesday = serializers.SerializerMethodField()
-    works_thursday = serializers.SerializerMethodField()
-    works_friday = serializers.SerializerMethodField()
-    works_saturday = serializers.SerializerMethodField()
-    works_sunday = serializers.SerializerMethodField()
-    grade = serializers.SerializerMethodField()
-    formatted_grade = serializers.SerializerMethodField()
-    country = serializers.SerializerMethodField()
-    country_name = serializers.SerializerMethodField()
-    formatted_location = serializers.SerializerMethodField()
-    profile_url = serializers.SerializerMethodField()
-    networks = serializers.CharField(source="network_codes")
-    formatted_networks = serializers.CharField()
-    additional_responsibilities = serializers.CharField()
-    formatted_additional_responsibilities = serializers.CharField()
-    key_skills = serializers.CharField(source="key_skill_codes")
-    learning_and_development = serializers.CharField()
-    formatted_learning_and_development = serializers.CharField()
-    professions = serializers.CharField(source="profession_codes")
-    formatted_professions = serializers.CharField()
-    completion_score = serializers.IntegerField(source="profile_completion")
-    city = serializers.CharField(source="town_city_or_region")
-    language_fluent = serializers.CharField(source="fluent_languages")
-    language_intermediate = serializers.CharField(source="intermediate_languages")
-    last_edited_or_confirmed_at = serializers.DateTimeField(source="updated_at")
-    people_finder_id = serializers.CharField(source="slug")
-    staff_sso_id = serializers.CharField(source="user.legacy_sso_user_id", default=None)
-    location_other_uk = serializers.CharField(source="regional_building")
-    location_other_overseas = serializers.CharField(source="international_building")
-    formatted_buildings = serializers.CharField()
-    formatted_key_skills = serializers.CharField()
-    other_additional_responsibilities = serializers.CharField(
-        source="other_additional_roles"
-    )
-    manager_people_finder_id = serializers.CharField(source="manager_id")
-    roles = TeamMemberSerialiser(many=True, read_only=True)
-    last_login_at = serializers.SerializerMethodField()
-    formatted_roles = serializers.CharField()
-    legacy_people_finder_id = serializers.CharField(source="legacy_slug")
-
-    def get_last_login_at(self, obj):
-        return obj.user and obj.user.last_login
-
-    def get_grade(self, obj):
-        if obj.grade:
-            return obj.grade.code
-        return ""
-
-    def get_formatted_grade(self, obj):
-        if obj.grade:
-            return obj.grade.name
-        return ""
-
-    def get_country(self, obj):
-        if obj.country:
-            return obj.country.code
-        else:
-            return ""
-
-    def get_country_name(self, obj):
-        if obj.country:
-            return obj.country.name
-        else:
-            return ""
-
-    def workday(self, obj, code):
-        if obj.workday_list:
-            return code in obj.workday_list
-        return False
-
-    def get_works_monday(self, obj):
-        return self.workday(obj, "mon")
-
-    def get_works_tuesday(self, obj):
-        return self.workday(obj, "tue")
-
-    def get_works_wednesday(self, obj):
-        return self.workday(obj, "wed")
-
-    def get_works_thursday(self, obj):
-        return self.workday(obj, "thu")
-
-    def get_works_friday(self, obj):
-        return self.workday(obj, "fri")
-
-    def get_works_saturday(self, obj):
-        return self.workday(obj, "sat")
-
-    def get_works_sunday(self, obj):
-        return self.workday(obj, "sun")
-
     class Meta:
         model = Person
         fields = [
+            "people_finder_id",
+            "staff_sso_id",
             "email",
             "contact_email",
+            "full_name",
+            "first_name",
+            "last_name",
+            "profile_url",
+            "roles",
+            "formatted_roles",
+            "manager_people_finder_id",
+            "completion_score",
+            "is_stale",
             "works_monday",
             "works_tuesday",
             "works_wednesday",
@@ -132,36 +52,24 @@ class PersonSerializer(serializers.ModelSerializer):
             "works_friday",
             "works_saturday",
             "works_sunday",
-            "location_in_building",
-            "city",
-            "country",
-            "language_fluent",
-            "language_intermediate",
-            "grade",
-            "created_at",
-            "last_edited_or_confirmed_at",
-            "login_count",
-            "last_login_at",
-            "people_finder_id",
-            "legacy_people_finder_id",
-            "staff_sso_id",
-            "full_name",
-            "first_name",
-            "last_name",
             "primary_phone_number",
             "secondary_phone_number",
             "formatted_location",
-            "location_other_uk",
-            "location_other_overseas",
             "buildings",
             "formatted_buildings",
-            "roles",
-            "formatted_roles",
+            "city",
+            "country",
+            "country_name",
+            "grade",
+            "formatted_grade",
+            "location_in_building",
+            "location_other_uk",
+            "location_other_overseas",
             "key_skills",
             "other_key_skills",
             "formatted_key_skills",
             "learning_and_development",
-            "other_learning_interests",
+            "other_learning_and_development",
             "formatted_learning_and_development",
             "networks",
             "formatted_networks",
@@ -170,19 +78,127 @@ class PersonSerializer(serializers.ModelSerializer):
             "additional_responsibilities",
             "other_additional_responsibilities",
             "formatted_additional_responsibilities",
-            "manager_people_finder_id",
-            "completion_score",
-            "profile_url",
-            "country_name",
-            "formatted_grade",
-            "is_stale",
+            "language_fluent",
+            "language_intermediate",
+            "created_at",
+            "last_edited_or_confirmed_at",
+            "login_count",
+            "last_login_at",
+            # New fields
+            "slug",
+            "manager_slug",
+            "legacy_people_finder_slug",
         ]
 
+    people_finder_id = serializers.IntegerField(source="pk")
+    staff_sso_id = serializers.CharField(source="user.legacy_sso_user_id", default=None)
+    profile_url = serializers.SerializerMethodField()
+    roles = TeamMemberSerialiser(many=True, read_only=True)
+    formatted_roles = serializers.ListField(child=serializers.CharField())
+    manager_people_finder_id = serializers.IntegerField(source="manager_id")
+    completion_score = serializers.IntegerField(source="profile_completion")
+    works_monday = serializers.SerializerMethodField()
+    works_tuesday = serializers.SerializerMethodField()
+    works_wednesday = serializers.SerializerMethodField()
+    works_thursday = serializers.SerializerMethodField()
+    works_friday = serializers.SerializerMethodField()
+    works_saturday = serializers.SerializerMethodField()
+    works_sunday = serializers.SerializerMethodField()
+    formatted_location = serializers.SerializerMethodField()
+    buildings = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="code"
+    )
+    formatted_buildings = serializers.CharField()
+    city = serializers.CharField(source="town_city_or_region")
+    country = serializers.SlugRelatedField(read_only=True, slug_field="code")
+    country_name = serializers.StringRelatedField(read_only=True, source="country")
+    grade = serializers.SlugRelatedField(read_only=True, slug_field="code")
+    formatted_grade = serializers.StringRelatedField(read_only=True, source="grade")
+    location_other_uk = serializers.CharField(source="regional_building")
+    location_other_overseas = serializers.CharField(source="international_building")
+    key_skills = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="code"
+    )
+    formatted_key_skills = serializers.CharField()
+    learning_and_development = serializers.SlugRelatedField(
+        many=True, read_only=True, source="learning_interests", slug_field="code"
+    )
+    other_learning_and_development = serializers.CharField(
+        source="other_learning_interests"
+    )
+    formatted_learning_and_development = serializers.CharField()
+    networks = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="code"
+    )
+    formatted_networks = serializers.CharField()
+    professions = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="code"
+    )
+    formatted_professions = serializers.CharField()
+    additional_responsibilities = serializers.SlugRelatedField(
+        many=True, read_only=True, source="additional_roles", slug_field="code"
+    )
+    other_additional_responsibilities = serializers.CharField(
+        source="other_additional_roles"
+    )
+    formatted_additional_responsibilities = serializers.CharField()
+    language_fluent = serializers.CharField(source="fluent_languages")
+    language_intermediate = serializers.CharField(source="intermediate_languages")
+    last_edited_or_confirmed_at = serializers.DateTimeField(
+        source="edited_or_confirmed_at"
+    )
+    last_login_at = serializers.SerializerMethodField()
+    # New fields
+    slug = serializers.CharField()
+    manager_slug = serializers.SlugRelatedField(
+        read_only=True, source="manager", slug_field="slug"
+    )
+    legacy_people_finder_slug = serializers.CharField(source="legacy_slug")
+
+    def get_last_login_at(self, obj):
+        return obj.user and obj.user.last_login
+
+    def _workday(self, obj, code):
+        if obj.workday_list:
+            return code in obj.workday_list
+
+        return False
+
+    def get_works_monday(self, obj):
+        return self._workday(obj, "mon")
+
+    def get_works_tuesday(self, obj):
+        return self._workday(obj, "tue")
+
+    def get_works_wednesday(self, obj):
+        return self._workday(obj, "wed")
+
+    def get_works_thursday(self, obj):
+        return self._workday(obj, "thu")
+
+    def get_works_friday(self, obj):
+        return self._workday(obj, "fri")
+
+    def get_works_saturday(self, obj):
+        return self._workday(obj, "sat")
+
+    def get_works_sunday(self, obj):
+        return self._workday(obj, "sun")
+
     def get_formatted_location(self, obj):
-        return f"{obj.location_in_building},  {obj.town_city_or_region}"
+        parts = (
+            obj.location_in_building,
+            *[x.name for x in obj.buildings.all()],
+            obj.town_city_or_region,
+        )
+
+        if not any(parts):
+            return None
+
+        return ", ".join(filter(None, parts))
 
     def get_profile_url(self, obj):
-        return obj.get_absolute_url()
+        return settings.BASE_URL + obj.get_absolute_url()
 
 
 class PersonPagination(pagination.CursorPagination):
@@ -205,18 +221,31 @@ class PersonViewSet(ReadOnlyModelViewSet):
     def get_queryset(self):
         queryset = (
             Person.objects.all()
-            .prefetch_related("roles")
-            .prefetch_related("roles__team")
             .select_related("country", "grade", "user", "manager")
+            .prefetch_related("roles", "roles__team")
+            # TODO: Most of these annotations are duplicates with the audit log person
+            # serializer query. We should refactor them into a queryset manager method.
             .annotate(
                 formatted_roles=ArrayAgg(
-                    "roles__team__name",
+                    Concat(
+                        "roles__job_title",
+                        Value(" in "),
+                        "roles__team__name",
+                        Case(
+                            When(
+                                roles__head_of_team=True, then=Value(" (head of team)")
+                            ),
+                            default=Value(""),
+                        ),
+                    ),
+                    filter=Q(roles__isnull=False),
                     distinct=True,
-                )
+                ),
             )
             .annotate(
-                network_codes=ArrayAgg(
-                    "networks__code",
+                formatted_buildings=StringAgg(
+                    "buildings__name",
+                    delimiter=", ",
                     distinct=True,
                 )
             )
@@ -228,34 +257,9 @@ class PersonViewSet(ReadOnlyModelViewSet):
                 )
             )
             .annotate(
-                formatted_buildings=StringAgg(
-                    "buildings__name",
-                    delimiter=", ",
-                    distinct=True,
-                )
-            )
-            .annotate(
-                building_codes=ArrayAgg(
-                    "buildings__code",
-                    distinct=True,
-                )
-            )
-            .annotate(
-                additional_responsibilities=ArrayAgg(
-                    "additional_roles__code",
-                    distinct=True,
-                )
-            )
-            .annotate(
                 formatted_additional_responsibilities=StringAgg(
                     "additional_roles__name",
-                    delimiter="' ",
-                    distinct=True,
-                )
-            )
-            .annotate(
-                key_skill_codes=ArrayAgg(
-                    "key_skills__code",
+                    delimiter=", ",
                     distinct=True,
                 )
             )
@@ -267,28 +271,16 @@ class PersonViewSet(ReadOnlyModelViewSet):
                 )
             )
             .annotate(
-                learning_and_development=ArrayAgg(
-                    "learning_interests__name",
-                    distinct=True,
-                )
-            )
-            .annotate(
                 formatted_learning_and_development=StringAgg(
-                    "learning_interests__code",
+                    "learning_interests__name",
                     delimiter=", ",
-                    distinct=True,
-                )
-            )
-            .annotate(
-                profession_codes=ArrayAgg(
-                    "professions__code",
                     distinct=True,
                 )
             )
             .annotate(
                 formatted_professions=StringAgg(
                     "professions__name",
-                    delimiter="' ",
+                    delimiter=", ",
                     distinct=True,
                 )
             )
@@ -302,4 +294,5 @@ class PersonViewSet(ReadOnlyModelViewSet):
             .defer("photo", "do_not_work_for_dit")
             .order_by("-created_at")
         )
+
         return queryset
