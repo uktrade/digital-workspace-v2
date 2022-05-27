23e7260560034bc79a050ae3f80c38df0a58ccc8
Ross Miller <github@decodedcollective.com>
2022-05-27 10:55:55 +0100

    Tidy up - remove Wordpress import and unused reqs

--- import_wordpress/parser/wagtail_content/theme.py
+++
@@ -1,9 +0,0 @@
-from django.template.defaultfilters import slugify
-
-from content.models import Theme
-
-
-def create_theme(theme):
-    Theme.objects.get_or_create(
-        title=theme["title"], slug=slugify(theme["title"]), summary=theme["excerpt"]
-    )
