d4b75efcb89fd89772d1d9b85ef3a8c5fabc4aab
Ross Miller <rossmiller@Rosss-MacBook-Pro.local>
2022-06-22 10:30:38 +0100

    Remove general "always eager"

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,1 @@
 from .env import *  # noqa
-
-
-# Turn on Celery
-CELERY_ALWAYS_EAGER = False
--- config/settings/test.py
+++ config/settings/test.py
@@ -5,3 +5,6 @@ APP_ENV = "test"
 
 # Required for tests to bypass SSO.
 MIDDLEWARE.remove("authbroker_client.middleware.ProtectAllViewsMiddleware")  # noqa
+
+# Turn off Celery
+CELERY_ALWAYS_EAGER = True
