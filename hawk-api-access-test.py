c714a924d74d2c192439c07594476fed138cabb7
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2022-11-22 11:35:09 +0000

    Bump cryptography

--- hawk-api-access-test.py
+++ hawk-api-access-test.py
@@ -4,34 +4,44 @@ import requests
 from mohawk import Sender
 
 
-# URL = "http://localhost:8000/peoplefinder/api/activity-stream/"
-# URL = "http://localhost:8000/peoplefinder/api/person-api/"
-URL = "http://localhost:8000/peoplefinder/api/team-api/"
+BASE_URL = "http://localhost:8000"
+PATH = "/peoplefinder/api/person-api/"
+
+URL = f"{BASE_URL}{PATH}"
 
 ID = "xxx"
 SECRET = "xxx"
 
-content_type = "application/json"
-content = ""
+url = URL
 
-sender = Sender(
-    {
-        "id": ID,
-        "key": SECRET,
-        "algorithm": "sha256",
-    },
-    URL,
-    "GET",
-    content=content,
-    content_type=content_type,
-)
+while True:
+    sender = Sender(
+        {
+            "id": ID,
+            "key": SECRET,
+            "algorithm": "sha256",
+        },
+        url,
+        "GET",
+        content="",
+        content_type="application/json",
+    )
 
-# print(sender.request_header)
+    response = requests.get(
+        url,
+        headers={
+            "Authorization": sender.request_header,
+            "Content-Type": "application/json",
+        },
+    )
 
-response = requests.get(
-    URL,
-    data=content,
-    headers={"Authorization": sender.request_header, "Content-Type": content_type},
-)
+    data = response.json()
+    print(json.dumps(data))
 
-print(json.dumps(response.json()))
+    # uncomment to only run once
+    # break
+
+    if data.get("next"):
+        url = data.get("next")
+    else:
+        break
