09b07aebeed06ac27bd514e631a9ee0cc5874bf1
marcelkornblum <me@marcelkornblum.com>
2023-02-15 15:35:28 +0000

    custom templatetag to write GIT_COMMIT, GIT_BRANCH and APP_ENV into an HTML comment

---
+++ core/templatetags/git_info.py
@@ -0,0 +1,15 @@
+import os
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.simple_tag
+def git_info():
+    APP_ENV = os.environ.get("APP_ENV", "-")
+    GIT_BRANCH = os.environ.get("GIT_BRANCH", "-")
+    GIT_COMMIT = os.environ.get("GIT_COMMIT", "-")
+    return mark_safe(
+        f"<!-- env:{APP_ENV} // git_branch:{GIT_BRANCH} // "
+        f"git_commit:{GIT_COMMIT} -->")
