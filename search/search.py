5d08d354dce03a8c23690c32699712ae50e4772b
marcelkornblum <me@marcelkornblum.com>
2023-02-20 16:43:43 +0000

    fixed up tweaks for repl convenience

--- search/search.py
+++ search/search.py
@@ -55,14 +55,16 @@ def _team_query(request: HttpRequest) -> QuerySet:
     return Team.objects.all()
 
 
-def sanitize_search_query(query = None) -> str:
+def sanitize_search_query(query: Optional[str] = None) -> str:
     if query is None:
         return ""
+
     # find all properly quoted substrings with matching opening and closing "|'
     matches = re.split(r"([\"'])(.*?)(\1)", query)
+
     output = ""
     quote_next_match = False
-    valid_quotes = ["\"", "'"]
+    valid_quotes = ['"', "'"]
     for match in matches:
         if match == "":
             continue
@@ -78,6 +80,8 @@ def sanitize_search_query(query = None) -> str:
         output += re.sub(r"[^a-zA-Z0-9-.~_\s]", "", match)
         if quote_next_match:
             output += "'"
+
     if not unicodedata.is_normalized("NFKD", output):
         output = unicodedata.normalize("NFKD", output)
+
     return output
