f655b7e06e702334216be516158f785222743896
marcelkornblum <me@marcelkornblum.com>
2023-02-21 11:17:21 +0000

    rewrite tests

--- search/test/test_search.py
+++ search/test/test_search.py
@@ -1,99 +1,77 @@
+import pytest
 from django.test import Client, TestCase
 
 from search.search import sanitize_search_query
 
 
-class TestSearchUtils(TestCase):
-    def test_sanitize_asciifolds_accented_chars(self):
-        query = "Gôod morninç øæå"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "Good morninc a")
-
-        query = "Mieux vaut être seul que mal accompagné"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "Mieux vaut etre seul que mal accompagne")
-
-        query = "Après la pluie, le beau temps"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "Apres la pluie le beau temps")
-
-        query = "À bon chat, bon rat"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "A bon chat bon rat")
-
-        query = "Lăpușneanu"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "Lapusneanu")
-
-        query = "Пожалуйста"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "")
-
+@pytest.mark.parametrize(
+    "query,result",
+    [
+        ("Gôod morninç øæå", "Good morninc a"),
+        (
+            "Mieux vaut être seul que mal accompagné",
+            "Mieux vaut etre seul que mal accompagne",
+        ),
+        ("Après la pluie, le beau temps", "Apres la pluie le beau temps"),
+        ("À bon chat, bon rat", "A bon chat bon rat"),
+        ("Lăpușneanu", "Lapusneanu"),
+        ("Пожалуйста", ""),
         # ensure composite chars are also folded
-        query = "Ç \u0043\u0327"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "C C")
+        ("Ç is the same as \u0043\u0327", "C is the same as C"),
+    ],
+)
+def test_sanitize_asciifolds_accented_chars(query, result):
+    assert sanitize_search_query(query) == result
 
-    def test_sanitize_returns_str_containing_only_safe_chars(self):
-        query = None
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "")
 
-        query = "¯\_(ツ)_/¯"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, " __ ")
+@pytest.mark.parametrize(
+    "query,result",
+    [
+        (None, ""),
+        ("¯\_(ツ)_/¯", " __ "),
+        ("!/(^&*)\\+{[@]}", ""),
+    ],
+)
+def test_sanitize_returns_str_containing_only_safe_chars(query, result):
+    assert sanitize_search_query(query) == result
 
-        query = "!/(^&*)\\+{[@]}"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "")
 
-    def test_sanitize_preserves_query_terms_and_phrases(self):
-        query = "query"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, query)
-
-        query = "a basic query"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, query)
-
-        query = "a 'more complex' query"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, query)
-
-        query = "a 'very complex' query with 'multiple phrases'"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, query)
-
+@pytest.mark.parametrize(
+    "query,result",
+    [
+        ("query", "query"),
+        ("a basic query", "a basic query"),
+        ("a 'more complex' query", "a 'more complex' query"),
+        (
+            "a 'very complex' query with 'multiple phrases'",
+            "a 'very complex' query with 'multiple phrases'",
+        ),
         # only preserve single level deep
-        query = "a 'very complex' query with '\"multiple nested\" phrases'"
-        result = sanitize_search_query(query)
-        self.assertEqual(
-            result, "a 'very complex' query with 'multiple nested phrases'"
-        )
+        (
+            "a 'very complex' query with '\"multiple nested\" phrases'",
+            "a 'very complex' query with 'multiple nested phrases'",
+        ),
+    ],
+)
+def test_sanitize_preserves_query_terms_and_phrases(query, result):
+    assert sanitize_search_query(query) == result
 
-    def test_sanitize_rationalises_valid_quotes(self):
-        query = "a 'phrased' query"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, query)
 
+@pytest.mark.parametrize(
+    "query,result",
+    [
+        ("a 'phrased' query", "a 'phrased' query"),
         # quote styles are rationalised
-        query = 'another "phrased" query'
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "another 'phrased' query")
-
-        query = 'a "misquoted query'
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "a misquoted query")
-
-        query = "another misquoted' query"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "another misquoted query")
-
+        ('another "phrased" query', "another 'phrased' query"),
+        ('a "misquoted query', "a misquoted query"),
+        ("another misquoted' query", "another misquoted query"),
         # it will always match the first and second quotes of the same type
-        query = "a misquoted' query with 'a phrase'"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "a misquoted' query with 'a phrase")
-
-        query = "a misquoted 'query with' 'several' 'phrases"
-        result = sanitize_search_query(query)
-        self.assertEqual(result, "a misquoted 'query with' 'several' phrases")
+        ("a misquoted' query with 'a phrase'", "a misquoted' query with 'a phrase"),
+        (
+            "a misquoted 'query with' 'several' 'phrases",
+            "a misquoted 'query with' 'several' phrases",
+        ),
+    ],
+)
+def test_sanitize_rationalises_valid_quotes(query, result):
+    assert sanitize_search_query(query) == result
