8d4c1f2755c2cb37f8aa727167cf7201d9859b4a
marcelkornblum <me@marcelkornblum.com>
2023-03-13 10:32:49 +0000

    linted files

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ from core import views
 urlpatterns = [
     path("problem-found/", views.page_problem_found, name="page_problem_found"),
     path("deactivated/", views.deactivated, name="deactivated"),
+    path("400/<exception>/", views.view_400),
+    path("403/<exception>/", views.view_403),
+    path("404/<exception>/", views.view_404),
+    path("500/", views.view_500),
 ]
