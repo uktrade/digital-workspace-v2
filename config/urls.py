463d4c519de4d12473486ed2df9c068ad942af34
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-03-29 11:33:47 +0100

    Add test view to login as one of the superusers

--- config/urls.py
+++ config/urls.py
@@ -46,7 +46,6 @@ urlpatterns = [
     path("", include(wagtail_urls)),
 ]
 
-
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -54,6 +53,24 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Test admin login
+    def login_as_admin(request):
+        from django.contrib.auth import login
+
+        from user.models import User
+
+        user = User.objects.filter(is_superuser=True).first()
+        login(
+            request,
+            user,
+            backend="django.contrib.auth.backends.ModelBackend",
+        )
+        return RedirectView.as_view(url="/")(request)
+
+    urlpatterns = [
+        path("login-as-admin/", login_as_admin),
+    ] + urlpatterns
 
 # Removed until we find a fix for Wagtail's redirect behaviour
 handler404 = "core.views.view_404"
