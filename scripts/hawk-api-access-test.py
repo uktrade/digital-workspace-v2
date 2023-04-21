be190c261ea914544323812b52b6e6c29e83a409
marcelkornblum <me@marcelkornblum.com>
2023-04-21 12:01:13 +0100

    hawk script moved

---
+++ scripts/hawk-api-access-test.py
@@ -0,0 +1,47 @@
+import json
+
+import requests
+from mohawk import Sender
+
+
+BASE_URL = "http://localhost:8000"
+PATH = "/peoplefinder/api/person-api/"
+
+URL = f"{BASE_URL}{PATH}"
+
+ID = "xxx"
+SECRET = "xxx"
+
+url = URL
+
+while True:
+    sender = Sender(
+        {
+            "id": ID,
+            "key": SECRET,
+            "algorithm": "sha256",
+        },
+        url,
+        "GET",
+        content="",
+        content_type="application/json",
+    )
+
+    response = requests.get(
+        url,
+        headers={
+            "Authorization": sender.request_header,
+            "Content-Type": "application/json",
+        },
+    )
+
+    data = response.json()
+    print(json.dumps(data))
+
+    # uncomment to only run once
+    # break
+
+    if data.get("next"):
+        url = data.get("next")
+    else:
+        break
