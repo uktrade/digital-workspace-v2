2ad93a8631a6779acafc5d06258fdea15f5c28f6
Luisella Strona <luisella21@hotmail.com>
2023-06-08 16:53:37 +0100

    Added rwad only field.

--- src/tools/models.py
+++ src/tools/models.py
@@ -1,13 +1,39 @@
+from django import forms
 from django.db import models
 from django.shortcuts import redirect
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 
 from content.models import ContentPage
 from working_at_dit.models import PageWithTopics
 
+class IrapDataAbstract(models.Model):
+    product_irap_reference_number = models.IntegerField(primary_key=True)
+    product_name = models.CharField(
+        max_length=2048,
+        null=True,
+        blank=True,
+    )
+    functionality = models.CharField(
+        max_length=2048,
+        null=True,
+        blank=True,
+    )
+    latest_accreditation_status = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True,
+    )
 
-class Tool(PageWithTopics):
+    class Meta:
+        abstract = True
+
+class IrapData(IrapDataAbstract):
+    pass
+
+
+class Tool(PageWithTopics, IrapDataAbstract):
     is_creatable = True
+    product_irap_reference_number = models.IntegerField(blank=True, null=True)
 
     redirect_url = models.CharField(
         null=True,
@@ -20,12 +46,29 @@ class Tool(PageWithTopics):
         max_length=2048,
     )
 
+    # using the correct widget for your field type and desired effect
+    readonly_widget = forms.TextInput(
+        attrs = {
+            'disabled': 'true'
+        }
+    )
+
     parent_page_types = ["tools.ToolsHome"]
     subpage_types = []  # Should not be able to create children
 
     content_panels = PageWithTopics.content_panels + [
         FieldPanel("redirect_url"),
         FieldPanel("long_description"),
+        MultiFieldPanel(
+            [
+                FieldPanel("product_irap_reference_number", widget=readonly_widget),
+                FieldPanel("product_name", widget=readonly_widget),
+                FieldPanel("functionality", widget=readonly_widget),
+            ],
+            heading="IRAP Fields",
+            classname="collapsed",
+        )
+
     ]
 
 
