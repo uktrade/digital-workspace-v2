efe57d51ee3c4770fc4b605397625e92fc3ec567
Luisella Strona <luisella21@hotmail.com>
2023-06-15 17:50:44 +0100

    Added models for IRAP tool data from DW

---
+++ src/tools/import_irap_tool.py
@@ -0,0 +1,60 @@
+from tools.models import IrapToolData, IrapToolDataImport
+
+def diff_irap_data(old: IrapToolData, new: IrapToolDataImport) -> tuple[[], bool]:
+    fields = new._meta.get_fields()
+    changed = False
+    changes = []
+    # TO DO safety checks that the two records have the same pk
+    for field in fields:
+        new_value = getattr(IrapToolDataImport, field.name)
+        old_value = getattr(IrapToolData, field.name)
+        if old_value != new_value:
+            changed = True
+            setattr(new, field.name, new_value)
+            changes.append(
+                {
+                    "action": "change",
+                    "key": field.name,
+                    "from_value": old_value,
+                    "to_value": new_value
+                }
+            )
+        return changed, changes
+
+
+def process_import():
+    """To be called after the irap data has been imported successfully from DW"""
+    # Clear all the relevant statuses on target table
+    # Set the status to deleted: each record in the import table
+    # is checked against the records in the irap table and the status is
+    # set accordingly. So if the records does not exist in the import table
+    # it has been deleted
+    # Set reviewed to False: it will be set to True when the administrator
+    # addresses the NEW, CHANGED and DELETED status
+    IrapToolData.objects.update(
+        after_import_status=IrapToolData.AfterImportStatus.DELETED,
+        processed=False,
+        changed_fields=None
+    )
+
+    imported_iraps = IrapToolDataImport.objects.all()
+    for imported_irap in imported_iraps:
+        irap, created = \
+            IrapToolData.objects.get_or_create(
+                product_irap_reference_number = imported_irap.product_irap_reference_number
+            )
+        if created:
+            irap.product_name = imported_irap.product_name
+            irap.functionality = imported_irap.functionality
+            irap.AfterImportStatus = IrapToolData.AfterImportStatus.NEW
+        else:
+            changed, changes = diff_irap_data( imported_irap,irap)
+            if changed:
+                irap.AfterImportStatus = IrapToolData.AfterImportStatus.CHANGED
+                irap.changed_fields = changes
+            else:
+                irap.AfterImportStatus = IrapToolData.AfterImportStatus.UNCHANGED
+                irap.reviewed = True
+        irap.save()
+
+
--- src/tools/models.py
+++ src/tools/models.py
@@ -1,18 +1,75 @@
+from django import forms
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.shortcuts import redirect
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, HelpPanel, MultiFieldPanel
 
 from content.models import ContentPage
 from working_at_dit.models import PageWithTopics
 
+class IrapToolDataAbstract(models.Model):
+    product_irap_reference_number = models.IntegerField(primary_key=True)
+    product_name = models.CharField(
+        max_length=2048,
+        null=True,
+        blank=True,
+    )
+    functionality = models.CharField(
+        max_length=2048,
+        null=True,
+        blank=True,
+    )
+    # TODO Add new fields that will be created later:  description and owner
+    class Meta:
+        abstract = True
+
+class IrapToolDataImport(IrapToolDataAbstract):
+    pass
+
+
+class IrapToolData(IrapToolDataAbstract):
+    class AfterImportStatus(models.TextChoices):
+        NEW = "new", "new"
+        UNCHANGED = "unchanged", "Unchanged"
+        CHANGED = "changed", "Changed"
+        DELETED = "deleted", "Deleted"
+    after_import_status = \
+        models.CharField(max_length=9,
+                         choices=AfterImportStatus.choices,
+                         default=AfterImportStatus.NEW)
+    # Processed is changed to TRUE
+    # when the content admin NEW, CHANGED and DELETED status
+    reviewed = models.BooleanField(default=False)
+    reviewed_date = models.DateTimeField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    changed_fields = models.JSONField(encoder=DjangoJSONEncoder, null=True, blank=True)
+
 
 class Tool(PageWithTopics):
     is_creatable = True
+    irap_tool = models.OneToOneField(
+        IrapToolData,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
 
     redirect_url = models.CharField(
         null=True,
         blank=True,
         max_length=2048,
+    )
+    long_description = models.CharField(
+        null=True,
+        blank=True,
+        max_length=2048,
+    )
+
+    # using the correct widget for your field type and desired effect
+    readonly_widget = forms.TextInput(
+        attrs = {
+            'disabled': 'true'
+        }
     )
 
     parent_page_types = ["tools.ToolsHome"]
@@ -20,7 +77,10 @@ class Tool(PageWithTopics):
 
     content_panels = PageWithTopics.content_panels + [
         FieldPanel("redirect_url"),
+        HelpPanel("long_description"),
+        FieldPanel("irap_tool", widget=readonly_widget),
     ]
+
 
     def serve(self, request):
         return redirect(self.redirect_url)
