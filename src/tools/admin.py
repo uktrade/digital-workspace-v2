efeee1749d53adb90946ac93854742bec1a440f9
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2023-06-29 12:04:47 +0100

    TOOLS 10 New models for irap (#376)
    - First pass at creating the new models required for importing IRAP tools from Data workspace.
    - Really small changes to templates, to allow viewing the Tools data.
    - Primitive admin interface, to be used for testing.

---
+++ src/tools/admin.py
@@ -0,0 +1,8 @@
+from core.admin import admin_site
+from tools.models import IrapToolData, IrapToolDataImport, Tool
+
+# Really crude admin, to be used for development
+
+admin_site.register(IrapToolData)
+admin_site.register(IrapToolDataImport)
+admin_site.register(Tool)
