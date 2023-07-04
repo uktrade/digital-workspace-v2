d4991e5dbe04f38655d95d72dffbffba7d68c666
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-07-04 14:33:51 +0100

    DWPF-589 Add edit links to the profile page

--- src/peoplefinder/templatetags/profile.py
+++ src/peoplefinder/templatetags/profile.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from django import template
+from django.urls import reverse
 from django.utils.safestring import SafeString, mark_safe
 
 from peoplefinder.utils import profile_completion_field_statuses
@@ -17,4 +18,17 @@ def profile_completion_field_tag(field_name, profile) -> Union[SafeString, str]:
     field_statuses = profile_completion_field_statuses(profile)
     if field_name in field_statuses and not field_statuses[field_name]:
         return tag
+    return ""
+
+
+@register.simple_tag
+def profile_completion_field_link(field_name, profile) -> Union[SafeString, str]:
+    human_readable_field_name = field_name.replace("_", " ")
+    profile_edit_url = reverse("profile-edit", args=[profile.slug])
+    edit_field_link = mark_safe(
+        f'<a class="govuk-link" href="{profile_edit_url}#core_field_id_{field_name}">Add {human_readable_field_name}</a>'
+    )
+    field_statuses = profile_completion_field_statuses(profile)
+    if field_name in field_statuses and not field_statuses[field_name]:
+        return edit_field_link
     return ""
