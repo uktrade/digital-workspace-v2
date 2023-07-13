ebdbb8953bbcd89b72c025b38487441cdad56946
marcelkornblum <me@marcelkornblum.com>
2023-07-13 11:46:03 +0100

    reset logging level

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -444,7 +444,7 @@ LOGGING = {
     },
     "root": {
         "handlers": ["stdout"],
-        "level": os.getenv("ROOT_LOG_LEVEL", "DEBUG"),
+        "level": os.getenv("ROOT_LOG_LEVEL", "INFO"),
     },
     "loggers": {
         "django": {
