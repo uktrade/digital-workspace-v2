e4ed8622f563e8c0a8732110013f18bd8afd0f0a
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-07-26 13:59:09 +0100

    Fix E2E tests (#406)

--- src/extended_search/apps.py
+++ src/extended_search/apps.py
@@ -1,13 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class ExtendedSearchConfig(AppConfig):
     name = "extended_search"
 
     def ready(self):
-        from extended_search.settings import extended_search_settings as search_settings
         import extended_search.signals  # noqa
+        from extended_search.settings import extended_search_settings as search_settings
 
         search_settings.initialise_field_dict()
         search_settings.initialise_env_dict()
-        search_settings.initialise_db_dict()
+        if settings.APP_ENV != "test":
+            search_settings.initialise_db_dict()
