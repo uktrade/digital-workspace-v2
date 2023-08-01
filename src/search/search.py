85666752bafdfc34707a59b9c49c86b09ce08f0e
marcelkornblum <me@marcelkornblum.com>
2023-08-01 14:44:09 +0100

    switch to sets

--- src/search/search.py
+++ src/search/search.py
@@ -131,11 +131,11 @@ class NewPeopleSearchVector(PeopleSearchVector):
     def search(self, query, *args, **kwargs):
         queryset = self.get_queryset()
         query_obj = get_search_query(PersonIndexManager, query, Person, *args, **kwargs)
-        results = self._wagtail_search(queryset, query_obj, *args, **kwargs)
-        results = list(results) + list(
+        results = set(self._wagtail_search(queryset, query_obj, *args, **kwargs))
+        autocomplete_results = set(
             self.get_queryset().autocomplete(query).annotate_score("_score")
         )
-        return results
+        return results | autocomplete_results
 
 
 class NewTeamsSearchVector(TeamsSearchVector):
