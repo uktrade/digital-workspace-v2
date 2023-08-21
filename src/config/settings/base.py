3a7576c0e4c269c922d5692479c0676a510752fe
marcelkornblum <me@marcelkornblum.com>
2023-08-21 11:41:32 +0100

    switch settings to en-gb and Europe/London TZ

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -236,8 +236,8 @@ AUTHENTICATION_BACKENDS = [
 LOGIN_URL = reverse_lazy("authbroker_client:login")
 LOGIN_REDIRECT_URL = "/"
 
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = "en-gb"
+TIME_ZONE = "Europe/London"
 USE_I18N = True
 USE_TZ = True
 
