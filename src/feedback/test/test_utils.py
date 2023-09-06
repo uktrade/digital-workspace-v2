32c42c2d6ed075ecdcc4a4752079f0be3ff23440
Nico Picchio <nicolapicchio@gmail.com>
2023-09-06 12:38:09 +0100

    adds code and tests to check for settings and email recipients

--- src/feedback/test/test_utils.py
+++ src/feedback/test/test_utils.py
@@ -1,5 +1,7 @@
+import pytest
 from django_feedback_govuk.models import BaseFeedback
 from feedback.utils import feedback_received_within
+from feedback.utils import send_feedback_notification
 
 
 def test_no_feedback_submitted(db):
@@ -20,3 +22,31 @@ def test_feedback_submitted_over_24hrs_ago(db, freezer):
 
     freezer.move_to("2023-09-03")
     assert not feedback_received_within()
+
+
+# CASE 1: Method raises an error when calling and no settings are set
+def test_send_feedback_notification_when_no_settings_are_set():
+    with pytest.raises(Exception):
+        send_feedback_notification()
+
+
+# CASE 2: Method doesn't raise an error when it is called with valid settings (Django docs -> override_settings)
+# def test_send_feedback_notification_with_valid_settings():
+#     ...
+
+
+# CASE 3: If the length of the email recipients is 0, an error is raised
+def test_send_feedback_notification_with_no_email_recipients():
+    with pytest.raises(ValueError):
+        send_feedback_notification()
+
+
+# CASE 4: If the length is 1, then the send_email_notification method is called once (mock + mock.patch() + make sure all parameters included are correct)
+# def test_send_feedback_email_notification_with_one_email_recipient():
+#     ...
+
+
+# CASE 5: If the length is more than 1, the send_email_notification method is called once
+# per email address, test to make sure it is called with the expected data the expected number of times
+# (this is the most complex test in this scenario so don't worry too much,
+# I have to look at an example every time I write these types of tests)
--- src/feedback/utils.py
+++ src/feedback/utils.py
@@ -13,22 +13,28 @@ def feedback_received_within(days=1):
 
 
 def send_feedback_notification():
+    if (
+        settings.GOVUK_NOTIFY_API_KEY is None
+        or settings.FEEDBACK_NOTIFICATION_EMAIL_RECIPIENTS is None
+        or settings.FEEDBACK_NOTIFICATION_EMAIL_TEMPLATE_ID is None
+        or settings.WAGTAILADMIN_BASE_URL is None
+    ):
+        raise TypeError("No settings provided")
+
     notification_client = NotificationsAPIClient(
         settings.GOVUK_NOTIFY_API_KEY,
     )
     email_recipients = settings.FEEDBACK_NOTIFICATION_EMAIL_RECIPIENTS
+    govuk_notify_template_id = settings.FEEDBACK_NOTIFICATION_EMAIL_TEMPLATE_ID
+    base_url = settings.WAGTAILADMIN_BASE_URL
 
     if len(email_recipients) < 1:
-        raise Exception("No feedback recipients configured")
+        raise ValueError("No feedback recipients configured")
 
     for email_recipient in email_recipients:
-        message = notification_client.send_email_notification(
+        notification_client.send_email_notification(
             email_address=email_recipient,
-            template_id=settings.FEEDBACK_NOTIFICATION_EMAIL_TEMPLATE_ID,
-            personalisation={
-                "feedback_url": settings.WAGTAIL_BASE_URL
-                + reverse("submitted-feedback")
-            },
+            template_id=govuk_notify_template_id,
+            personalisation={"feedback_url": base_url + reverse("submitted-feedback")},
         )
-
-    return message
+    return
