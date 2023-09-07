4e14c87bf6bc7ea20019ea085444c700a8c44aee
Nico Picchio <nicolapicchio@gmail.com>
2023-09-07 12:27:37 +0100

    modifies tests for conciseness

--- src/feedback/test/test_utils.py
+++ src/feedback/test/test_utils.py
@@ -6,13 +6,9 @@ from django_feedback_govuk.models import BaseFeedback
 from feedback.utils import feedback_received_within, send_feedback_notification
 
 
-def test_no_feedback_submitted(db):
-    # feedback_received_within() returns False if no feedback was submitted.
+def test_no_feedback_submitted_and_feedback_submitted_within_24hrs(db):
+    # feedback_received_within() returns False if no feedback was submitted and True if feedback was submitted within the last 24hrs.
     assert not feedback_received_within()
-
-
-def test_feedback_submitted_within_24hr_window(db):
-    # feedback_received_within() returns True if feedback was submitted within the last 24hrs
     BaseFeedback.objects.create()
     assert feedback_received_within()
 
@@ -21,7 +17,8 @@ def test_feedback_submitted_over_24hrs_ago(db, freezer):
     # feedback_received_within() returns False if feedback was submitted over 24hrs ago
     freezer.move_to("2023-09-01")
     BaseFeedback.objects.create()
-
+    assert feedback_received_within()
+    
     freezer.move_to("2023-09-03")
     assert not feedback_received_within()
 
