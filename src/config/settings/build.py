146f13b494fdb466533c40710238fa305f080a02
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-09-22 17:12:19 +0100

    Delete comented out stuff

--- src/config/settings/build.py
+++ src/config/settings/build.py
@@ -1,9 +1,5 @@
 from .base import *  # noqa
 
-# AUTHBROKER_CLIENT_ID = "value-does-not-matter-for-build"
-# AUTHBROKER_CLIENT_SECRET = "value-does-not-matter-for-build"
-# AUTHBROKER_URL = "value-does-not-matter-for-build"
-
 SECRET_KEY = "value-does-not-matter-for-build"
 
 ELASTICSEARCH_DSL = {
