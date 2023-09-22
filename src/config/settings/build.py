82f01890e3f56afc7be168d9e361da7240ed08b9
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-09-22 15:46:21 +0100

    Add secret key, lose authbroker stuff

--- src/config/settings/build.py
+++ src/config/settings/build.py
@@ -1,5 +1,7 @@
 from .base import *  # noqa
 
-AUTHBROKER_CLIENT_ID = "value-does-not-matter-for-build"
-AUTHBROKER_CLIENT_SECRET = "value-does-not-matter-for-build"
-AUTHBROKER_URL = "value-does-not-matter-for-build"
+# AUTHBROKER_CLIENT_ID = "value-does-not-matter-for-build"
+# AUTHBROKER_CLIENT_SECRET = "value-does-not-matter-for-build"
+# AUTHBROKER_URL = "value-does-not-matter-for-build"
+
+SECRET_KEY = "value-does-not-matter-for-build"
