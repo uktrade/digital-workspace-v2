2c088c72d807774cc929c8420bfc636037479ed1
Anthony Roy <anthony.roy@digital.trade.gov.uk>
2023-09-26 16:48:51 +0100

    Removed elastic search app and config

--- src/config/settings/env.py
+++ src/config/settings/env.py
@@ -21,16 +21,16 @@ SESSION_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 INSTALLED_APPS += [  # noqa F405
-    "elasticapm.contrib.django",
+    #  "elasticapm.contrib.django",
 ]
 
-ELASTIC_APM = {
-    "SERVICE_NAME": "Digital Workspace",
-    "SECRET_TOKEN": env("ELASTIC_APM_SECRET_TOKEN"),  # noqa F405
-    "SERVER_URL": env("ELASTIC_APM_SERVER_URL"),  # noqa F405
-    "ENVIRONMENT": env("APP_ENV"),  # noqa F405
-    "SERVER_TIMEOUT": env("ELASTIC_APM_SERVER_TIMEOUT", default="20s"),  # noqa F405
-}
+#  ELASTIC_APM = {
+    #  "SERVICE_NAME": "Digital Workspace",
+    #  "SECRET_TOKEN": env("ELASTIC_APM_SECRET_TOKEN"),  # noqa F405
+    #  "SERVER_URL": env("ELASTIC_APM_SERVER_URL"),  # noqa F405
+    #  "ENVIRONMENT": env("APP_ENV"),  # noqa F405
+    #  "SERVER_TIMEOUT": env("ELASTIC_APM_SERVER_TIMEOUT", default="20s"),  # noqa F405
+#  }
 
 LOGGING = {
     "version": 1,
