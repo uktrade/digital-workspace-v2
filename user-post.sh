61e8be925b4d18879f2311b7b5d1eea142471f20
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-09-26 12:34:42 +0100

    Back out a directory before we try and move it back

--- user-post.sh
+++ user-post.sh
@@ -4,10 +4,17 @@ echo "Running post build script"
 pip install -r requirements.txt
 npm ci
 
+echo "Renaming .env.ci to .env"
+ls -al
 mv ".env.ci" ".env"
+ls -al
 
 cd src
 
 python manage.py collectstatic --settings=config.settings.test --noinput
 
+echo "Renaming .env to .env.ci"
+cd ../
+ls -al
 mv ".env" ".env.ci"
+ls -al
