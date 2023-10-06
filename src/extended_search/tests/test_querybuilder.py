f95d7660a5b905a2bbcb7608f32a3fa192567368
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-10-06 13:11:33 +0100

    Clean up search (#474)

--- src/extended_search/tests/test_querybuilder.py
+++ src/extended_search/tests/test_querybuilder.py
@@ -1,13 +1,13 @@
-import pytest
-
 from unittest.mock import call
+
+import pytest
 from wagtail.search.query import Boost, Fuzzy, Phrase, PlainText
 
 from content.models import ContentPage
 from extended_search.backends.query import OnlyFields
 from extended_search.managers import get_indexed_field_name, get_search_query
 from extended_search.managers.index import ModelIndexManager
-from extended_search.managers.query_builder import QueryBuilder
+from extended_search.managers.query_builder import NestedQueryBuilder, QueryBuilder
 from extended_search.models import Setting
 from extended_search.settings import extended_search_settings
 from extended_search.types import AnalysisType, SearchQueryType
@@ -51,47 +51,49 @@ class TestManagerInit:
 
 
 class TestQueryBuilder:
+    query_builder_class = QueryBuilder
+
     def test_get_inner_searchquery_for_querytype_doesnt_use_and_if_single_word(self):
         assert (
-            QueryBuilder._get_inner_searchquery_for_querytype(
+            self.query_builder_class._get_inner_searchquery_for_querytype(
                 "searchquery", SearchQueryType.QUERY_AND
             )
             is None
         )
-        result = QueryBuilder._get_inner_searchquery_for_querytype(
+        result = self.query_builder_class._get_inner_searchquery_for_querytype(
             "search query", SearchQueryType.QUERY_AND
         )
         assert result is not None
 
     def test_get_inner_searchquery_for_querytype_handles_searchquerytypes(self):
-        result = QueryBuilder._get_inner_searchquery_for_querytype(
+        result = self.query_builder_class._get_inner_searchquery_for_querytype(
             "search query", SearchQueryType.PHRASE
         )
         assert isinstance(result, Phrase)
         assert result.query_string == "search query"
 
-        result = QueryBuilder._get_inner_searchquery_for_querytype(
+        result = self.query_builder_class._get_inner_searchquery_for_querytype(
             "search query", SearchQueryType.QUERY_AND
         )
         assert isinstance(result, PlainText)
         assert result.operator == "and"
         assert result.query_string == "search query"
 
-        result = QueryBuilder._get_inner_searchquery_for_querytype(
+        result = self.query_builder_class._get_inner_searchquery_for_querytype(
             "search query", SearchQueryType.QUERY_OR
         )
         assert isinstance(result, PlainText)
         assert result.operator == "or"
         assert result.query_string == "search query"
 
-        result = QueryBuilder._get_inner_searchquery_for_querytype(
+        result = self.query_builder_class._get_inner_searchquery_for_querytype(
             "search query", SearchQueryType.FUZZY
         )
         assert isinstance(result, Fuzzy)
         assert result.query_string == "search query"
 
         with pytest.raises(ValueError):
-            QueryBuilder._get_inner_searchquery_for_querytype(
+            self.query_builder_class._get_inner_searchquery_for_querytype(
                 "search query", "anything"  # type: ignore
             )
 
@@ -124,53 +126,53 @@ class TestQueryBuilder:
         assert (query_boost * analyzer_boost * field_boost) != (query_boost)
         assert (query_boost * analyzer_boost * field_boost) != (analyzer_boost)
         assert (query_boost * analyzer_boost * field_boost) != (field_boost)
-        assert QueryBuilder._get_boost_for_field_querytype_analysistype(
-            "title", ContentPage, SearchQueryType.PHRASE, AnalysisType.EXPLICIT, {}
+        assert self.query_builder_class._get_boost_for_field_querytype_analysistype(
+            "title", ContentPage, SearchQueryType.PHRASE, AnalysisType.EXPLICIT
         ) == (query_boost * analyzer_boost * field_boost)
 
         query_boost = float(
             extended_search_settings["boost_parts"]["query_types"]["query_and"]
         )
-        assert QueryBuilder._get_boost_for_field_querytype_analysistype(
-            "title", ContentPage, SearchQueryType.QUERY_AND, AnalysisType.EXPLICIT, {}
+        assert self.query_builder_class._get_boost_for_field_querytype_analysistype(
+            "title", ContentPage, SearchQueryType.QUERY_AND, AnalysisType.EXPLICIT
         ) == (query_boost * analyzer_boost * field_boost)
 
         query_boost = float(
             extended_search_settings["boost_parts"]["query_types"]["query_or"]
         )
-        assert QueryBuilder._get_boost_for_field_querytype_analysistype(
-            "title", ContentPage, SearchQueryType.QUERY_OR, AnalysisType.EXPLICIT, {}
+        assert self.query_builder_class._get_boost_for_field_querytype_analysistype(
+            "title", ContentPage, SearchQueryType.QUERY_OR, AnalysisType.EXPLICIT
         ) == (query_boost * analyzer_boost * field_boost)
 
         query_boost = float(
             extended_search_settings["boost_parts"]["query_types"]["fuzzy"]
         )
-        assert QueryBuilder._get_boost_for_field_querytype_analysistype(
-            "title", ContentPage, SearchQueryType.FUZZY, AnalysisType.EXPLICIT, {}
+        assert self.query_builder_class._get_boost_for_field_querytype_analysistype(
+            "title", ContentPage, SearchQueryType.FUZZY, AnalysisType.EXPLICIT
         ) == (query_boost * analyzer_boost * field_boost)
 
         analyzer_boost = float(
             extended_search_settings["boost_parts"]["analyzers"]["tokenized"]
         )
-        assert QueryBuilder._get_boost_for_field_querytype_analysistype(
-            "title", ContentPage, SearchQueryType.FUZZY, AnalysisType.TOKENIZED, {}
+        assert self.query_builder_class._get_boost_for_field_querytype_analysistype(
+            "title", ContentPage, SearchQueryType.FUZZY, AnalysisType.TOKENIZED
         ) == (query_boost * analyzer_boost * field_boost)
 
         analyzer_boost = float(
             extended_search_settings["boost_parts"]["analyzers"]["explicit"]
         )
-        assert QueryBuilder._get_boost_for_field_querytype_analysistype(
-            "title", ContentPage, SearchQueryType.FUZZY, AnalysisType.KEYWORD, {}
+        assert self.query_builder_class._get_boost_for_field_querytype_analysistype(
+            "title", ContentPage, SearchQueryType.FUZZY, AnalysisType.KEYWORD
         ) == (query_boost * analyzer_boost * field_boost)
 
         with pytest.raises(ValueError):
-            QueryBuilder._get_boost_for_field_querytype_analysistype(
-                "title", ContentPage, "anything", AnalysisType.KEYWORD, {}
+            self.query_builder_class._get_boost_for_field_querytype_analysistype(
+                "title", ContentPage, "anything", AnalysisType.KEYWORD
             )
 
         with pytest.raises(ValueError):
-            QueryBuilder._get_boost_for_field_querytype_analysistype(
-                "title", ContentPage, SearchQueryType.FUZZY, "anything", {}
+            self.query_builder_class._get_boost_for_field_querytype_analysistype(
+                "title", ContentPage, SearchQueryType.FUZZY, "anything"
             )
 
     def test_get_searchquery_for_etc_uses_submethods(self, mocker):
@@ -180,11 +182,8 @@ class TestQueryBuilder:
         mock_boost = mocker.patch(
             "extended_search.managers.query_builder.QueryBuilder._get_boost_for_field_querytype_analysistype"
         )
-        mock_name = mocker.patch(
-            "extended_search.managers.query_builder.get_indexed_field_name"
-        )
         mock_query.return_value = None
-        result = QueryBuilder._get_searchquery_for_query_field_querytype_analysistype(
+        result = self.query_builder_class._get_searchquery_for_query_field_querytype_analysistype(
             "query",
             ContentPage,
             "title",
@@ -197,8 +196,7 @@ class TestQueryBuilder:
 
         mock_query.return_value = Phrase("query")
         mock_boost.return_value = 333.33
-        mock_name.return_value = "foo_bar_baz"
-        result = QueryBuilder._get_searchquery_for_query_field_querytype_analysistype(
+        result = self.query_builder_class._get_searchquery_for_query_field_querytype_analysistype(
             "query",
             ContentPage,
             "title",
@@ -207,7 +205,7 @@ class TestQueryBuilder:
             {},
         )
         assert isinstance(result, OnlyFields)
-        assert result.fields == ["foo_bar_baz"]
+        assert result.fields == ["title"]
         subquery = result.subquery
         assert isinstance(subquery, Boost)
         assert subquery.boost == 333.33
@@ -215,21 +213,20 @@ class TestQueryBuilder:
         assert subquery == mock_query.return_value
         mock_query.assert_called_with("query", SearchQueryType.PHRASE)
         mock_boost.assert_called_with(
-            "title", ContentPage, SearchQueryType.PHRASE, AnalysisType.EXPLICIT, {}
+            "title", ContentPage, SearchQueryType.PHRASE, AnalysisType.EXPLICIT
         )
-        mock_name.assert_called_with("title", AnalysisType.EXPLICIT)
 
-    def test_add_to_query(self):
+    def test_combine_queries(self):
         # using sets because the | operator is used to join querysets, and it's also vaslid to use on sets
-        assert QueryBuilder._add_to_query(
+        assert self.query_builder_class._combine_queries(
             set(["a", "b"]),
             None,
         ) == set(["a", "b"])
-        assert QueryBuilder._add_to_query(
+        assert self.query_builder_class._combine_queries(
             None,
             set(["a", "b"]),
         ) == set(["a", "b"])
-        assert QueryBuilder._add_to_query(
+        assert self.query_builder_class._combine_queries(
             set(["a", "b"]),
             set(["c", "d"]),
         ) == set(["a", "b", "c", "d"])
@@ -253,7 +250,9 @@ class TestQueryBuilder:
         )
         mapping = {}
         assert (
-            QueryBuilder._get_search_query_from_mapping("query", ContentPage, mapping)
+            self.query_builder_class._get_search_query_from_mapping(
+                "query", ContentPage, mapping
+            )
             is None
         )
         mock_query.assert_not_called()
@@ -265,7 +264,7 @@ class TestQueryBuilder:
             "fuzzy": [],
             "model_field_name": "--name--",
         }
-        result = QueryBuilder._get_search_query_from_mapping(
+        result = self.query_builder_class._get_search_query_from_mapping(
             "query", ContentPage, mapping
         )
         assert result == set(["--query--"])
@@ -299,7 +298,7 @@ class TestQueryBuilder:
             "search": [AnalysisType.EXPLICIT],
             "model_field_name": "--name--",
         }
-        result = QueryBuilder._get_search_query_from_mapping(
+        result = self.query_builder_class._get_search_query_from_mapping(
             "query", ContentPage, mapping
         )
         assert mock_query.call_count == 3
@@ -338,7 +337,7 @@ class TestQueryBuilder:
             "search": [AnalysisType.TOKENIZED],
             "model_field_name": "--name--",
         }
-        result = QueryBuilder._get_search_query_from_mapping(
+        result = self.query_builder_class._get_search_query_from_mapping(
             "query", ContentPage, mapping
         )
         assert mock_query.call_count == 3
@@ -377,10 +376,75 @@ class TestQueryBuilder:
             "search": [AnalysisType.TOKENIZED, AnalysisType.EXPLICIT],
             "model_field_name": "--name--",
         }
-        result = QueryBuilder._get_search_query_from_mapping(
+        result = self.query_builder_class._get_search_query_from_mapping(
             "query", ContentPage, mapping
         )
         assert mock_query.call_count == 6
+
+
+class TestNestedQueryBuilder(TestQueryBuilder):
+    query_builder_class = NestedQueryBuilder
+
+    def test_get_searchquery_for_etc_uses_submethods(self, mocker):
+        mock_query = mocker.patch(
+            "extended_search.managers.query_builder.NestedQueryBuilder._get_inner_searchquery_for_querytype"
+        )
+        mock_boost = mocker.patch(
+            "extended_search.managers.query_builder.NestedQueryBuilder._get_boost_for_field_querytype_analysistype"
+        )
+        mock_query.return_value = None
+        result = self.query_builder_class._get_searchquery_for_query_field_querytype_analysistype(
+            "query",
+            ContentPage,
+            "title",
+            SearchQueryType.PHRASE,
+            AnalysisType.EXPLICIT,
+            {},
+        )
+        assert result is None
+        mock_query.assert_called_once_with("query", SearchQueryType.PHRASE)
+
+        mock_query.return_value = Phrase("query")
+        mock_boost.return_value = 333.33
+        result = self.query_builder_class._get_searchquery_for_query_field_querytype_analysistype(
+            "query",
+            ContentPage,
+            "title",
+            SearchQueryType.PHRASE,
+            AnalysisType.EXPLICIT,
+            {"parent_model_field": "foo"},
+        )
+        assert isinstance(result, OnlyFields)
+        assert result.fields == ["foo.title"]
+        subquery = result.subquery
+        assert isinstance(subquery, Boost)
+        assert subquery.boost == 333.33
+        subquery = subquery.subquery
+        assert subquery == mock_query.return_value
+        mock_query.assert_called_with("query", SearchQueryType.PHRASE)
+        mock_boost.assert_called_with(
+            "foo.title", ContentPage, SearchQueryType.PHRASE, AnalysisType.EXPLICIT
+        )
+
+    @pytest.mark.django_db
+    def test_get_search_query_from_mapping_uses_settings_and_submethods_for_related_fields(
+        self, mocker
+    ):
+        query_outputs = [
+            set(["--query--"]),
+            set(["--query-2--"]),
+            set(["--query-3--"]),
+            set(["--query-4--"]),
+            set(["--query-5--"]),
+            set(["--query-6--"]),
+        ]
+        mock_query = mocker.patch(
+            "extended_search.managers.query_builder.NestedQueryBuilder._get_searchquery_for_query_field_querytype_analysistype",
+            side_effect=query_outputs,
+        )
+        mocker.patch(
+            "extended_search.managers.query_builder.Nested",
+        )
 
         # RELATED
         mock_query.reset_mock()
@@ -401,7 +465,7 @@ class TestQueryBuilder:
             "model_field_name": "--model-field-name--",
             "name": "--name--",
         }
-        result = QueryBuilder._get_search_query_from_mapping(
+        self.query_builder_class._get_search_query_from_mapping(
             "query", ContentPage, mapping
         )
         assert mock_query.call_count == 6  # tokenized => 3, x2 fields
