48cc1e5962e018a16f3db68029af65b97bbd046f
marcelkornblum <me@marcelkornblum.com>
2023-10-19 12:14:36 +0100

    connect to speculatively useful sso endpoint

--- src/user/management/commands/archive_users.py
+++ src/user/management/commands/archive_users.py
@@ -1,3 +1,8 @@
+from datetime import timedelta
+import requests
+from urllib.parse import urlparse, urlunparse
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
@@ -10,13 +15,13 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("days", type=int, default=90)
-        parser.add_argument("batch_size", type=int, default=25)
-        parser.add_argument("batch_start", type=int, default=0)
+        parser.add_argument("limit", type=int, default=25)
+        parser.add_argument("offset", type=int, default=0)
 
     def handle(self, *args, **options):
         days = options["days"]
-        batch_size = options["batch_size"]
-        batch_start = options["batch_start"]
+        limit = options["limit"]
+        offset = options["offset"]
 
         self.sync_inactive_users_profiles()
 
@@ -26,7 +31,7 @@ class Command(BaseCommand):
         )
         number_of_users = users_to_check.count()
 
-        if batch_size == 0:
+        if limit == 0:
             batch = users_to_check
             self.stdout.write(
                 self.style.NOTICE(
@@ -34,30 +39,43 @@ class Command(BaseCommand):
                 )
             )
         else:
-            batch = users_to_check[batch_start:batch_size]
+            batch = users_to_check[offset:limit]
             self.stdout.write(
                 self.style.NOTICE(
-                    f"Checking {batch_size} (from record {batch_start}) of {number_of_users} User records with last_login older than {days} ago"
+                    f"Checking {limit} (from record {offset}) of {number_of_users} User records with last_login older than {days} ago"
                 )
             )
 
         deactivated = 0
         ignored = 0
+        # check against SSO API endpoint to see if user is active there
+        authbroker_url = urlparse(settings.AUTHBROKER_URL)
+        url = urlunparse(authbroker_url._replace(path="/introspect/"))
+        headers = {"Authorization": f"bearer {settings.AUTHBROKER_INTROSPECTION_TOKEN}"}
+
         for user in batch:
-            # check against SSO API endpoint to see if user is active there
-            # if sso record is inactive:
-            # user.is_active = False
-            # user.save()
-            # deactivated += 1
-            # else:
-            # ignored += 1
-            ...
+            params = {"email": user.email}
+            response = requests.get(url, params, headers=headers, timeout=5)
+            if response.status_code == 200:
+                resp_json = response.json()
+                if not resp_json["is_active"]:
+                    user.is_active = False
+                    user.save()
+                    deactivated += 1
+                else:
+                    ignored += 1
+            else:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f"SSO introspect endpoint returned {response.status_code} status code for {user.email}"
+                    )
+                )
 
         self.stdout.write(
             self.style.SUCCESS(
                 "Job finished successfully\n"
-                f"{self.deactivated} User records marked as inactive\n"
-                f"{self.ignored} User records left as active\n"
+                f"{deactivated} User records marked as inactive\n"
+                f"{ignored} User records left as active\n"
             )
         )
 
