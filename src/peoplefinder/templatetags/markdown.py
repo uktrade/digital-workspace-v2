d9e9deac521f5d4cb47454633ff0b19db4674865
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-11-01 12:42:36 +0000

    DWPF-862 Prevent using javascript as a url in team description markdown

--- src/peoplefinder/templatetags/markdown.py
+++ src/peoplefinder/templatetags/markdown.py
@@ -3,12 +3,15 @@ from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
-
 register = template.Library()
 
 
 @register.filter(needs_autoescape=True)
-def markdown(text, autoescape=True):
+def markdown(text: str, autoescape=True):
+    # Remove any javascript: or data: from the text to prevent XSS
+    text = text.replace("javascript:", "")
+    text = text.replace("data:", "")
+
     if autoescape:
         esc = conditional_escape
     else:
