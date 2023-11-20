2ae05202dc4fb6f5c5cac88dcf978e1de1baa7f4
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-11-20 10:22:43 +0000

    Move Variable class alongside QueryBuilder logic

--- src/extended_search/managers/query_builder.py
+++ src/extended_search/managers/query_builder.py
@@ -15,12 +15,37 @@ from extended_search.index import (
     SearchField,
     get_indexed_models,
 )
-from extended_search.query import Variable
 from extended_search.settings import extended_search_settings as search_settings
 from extended_search.settings import get_settings_field_key
 from extended_search.types import AnalysisType, SearchQueryType
 
 logger = logging.getLogger(__name__)
+
+
+class Variable:
+    def __init__(self, name: str, query_type: SearchQueryType) -> None:
+        self.name = name
+        self.query_type = query_type
+
+    def output(self, query_str: str):
+        # split can be super basic since we don't support advanced search
+        query_parts = query_str.split()
+        match self.query_type:
+            case SearchQueryType.PHRASE:
+                query = Phrase(query_str)
+            case SearchQueryType.QUERY_AND:
+                # check the query_str merits an AND - does it contain multiple words?
+                if len(query_parts) > 1:
+                    query = PlainText(query_str, operator="and")
+                else:
+                    query = None
+            case SearchQueryType.QUERY_OR:
+                query = PlainText(query_str, operator="or")
+            case SearchQueryType.FUZZY:
+                query = Fuzzy(query_str)
+            case _:
+                raise ValueError(f"{self.query_type} must be a valid SearchQueryType")
+        return query
 
 
 class QueryBuilder:
