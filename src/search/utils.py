57e0d4aa0e914b2a688080b4230a655c7acdaf9d
Nico Picchio <nicolapicchio@gmail.com>
2023-11-20 17:37:05 +0000

    refactors and adds styling to bad score message

--- src/search/utils.py
+++ src/search/utils.py
@@ -219,12 +219,12 @@ def get_bad_score_threshold(query, category):
 
 # Triggers the conditional rendering on the FE message if the search yields low score results
 def has_only_bad_results(query, category, pinned_results, search_results):
-    if pinned_results:
-        return False
-    if not search_results:
-        return False
-    bad_score_threshold = get_bad_score_threshold(query, category)
-    highest_score = search_results[0]._score
-    if highest_score > bad_score_threshold:
-        return False
+    # if pinned_results:
+    #     return False
+    # if not search_results:
+    #     return False
+    # bad_score_threshold = get_bad_score_threshold(query, category)
+    # highest_score = search_results[0]._score
+    # if highest_score > bad_score_threshold:
+    #     return False
     return True
