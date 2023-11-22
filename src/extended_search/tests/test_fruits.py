5f0fde576575599ccedb614e9a7b7db5cbb1acd2
marcelkornblum <me@marcelkornblum.com>
2023-11-22 11:07:54 +0000

    supercharge fruit tests

--- src/extended_search/tests/test_fruits.py
+++ src/extended_search/tests/test_fruits.py
@@ -8,6 +8,7 @@ from user.models import User
 from user.test.factories import UserFactory
 from working_at_dit.models import PoliciesAndGuidanceHome
 from working_at_dit.tests.factories import GuidanceFactory, HowDoIFactory, PolicyFactory
+from peoplefinder.test.factories import PersonFactory, TeamFactory
 
 
 class TestFruits(TestCase):
@@ -29,9 +30,21 @@ class TestFruits(TestCase):
                 content_owner=self.content_owner.profile,
                 content_contact_email=self.content_owner.email,
             ),
+            GuidanceFactory.create(
+                parent=policies_and_guidance_home,
+                title="An irrelevant page",
+                content_owner=self.content_owner.profile,
+                content_contact_email=self.content_owner.email,
+            ),
             PolicyFactory.create(
                 parent=policies_and_guidance_home,
                 title="What fruit can I eat?",
+                content_owner=self.content_owner.profile,
+                content_contact_email=self.content_owner.email,
+            ),
+            PolicyFactory.create(
+                parent=policies_and_guidance_home,
+                title="Policies not relating to food",
                 content_owner=self.content_owner.profile,
                 content_contact_email=self.content_owner.email,
             ),
@@ -40,14 +53,41 @@ class TestFruits(TestCase):
                 content_owner=self.content_owner.profile,
                 content_contact_email=self.content_owner.email,
             ),
+            HowDoIFactory.create(
+                title="How to write test content",
+                content_owner=self.content_owner.profile,
+                content_contact_email=self.content_owner.email,
+            ),
         ]
         self.pages = [
             ToolFactory.create(
                 title="Fruit finder",
             ),
+            ToolFactory.create(
+                title="Internal tooling tool",
+            ),
             NewsPageFactory.create(
                 title="Big fruit news",
             ),
+            NewsPageFactory.create(
+                title="New news",
+            ),
+        ]
+        self.people = [
+            PersonFactory.create(
+                first_name="Martin",
+                last_name="Fruit",
+                email="martin.fruit@example.com",
+            ),
+            PersonFactory.create(
+                first_name="Adam", last_name="Ant", email="adam.ant@example.com"
+            ),
+        ]
+        self.teams = [
+            TeamFactory.create(
+                name="Fruit pickers", slug="fruit-pickers", abbreviation="FRUIT"
+            ),
+            TeamFactory.create(name="Middle managers", slug="managers"),
         ]
 
         call_command("update_index")
@@ -63,6 +103,10 @@ class TestFruits(TestCase):
             self.assertNotContains(response, page.title)
         for page in self.pages:
             self.assertNotContains(response, page.title)
+        for person in self.people:
+            self.assertNotContains(response, person.email)
+        for team in self.teams:
+            self.assertNotContains(response, team.name)
 
         self.assertContains(response, "All&nbsp;(0)")
         self.assertContains(response, "People&nbsp;(0)")
@@ -70,19 +114,162 @@ class TestFruits(TestCase):
         self.assertContains(response, "Guidance&nbsp;(0)")
         self.assertContains(response, "Tools&nbsp;(0)")
         self.assertContains(response, "News&nbsp;(0)")
+        self.assertContains(response, "0 pages")
+        self.assertContains(response, "0 people")
+        self.assertContains(response, "0 teams")
+        self.assertContains(response, "There are no matching pages.")
+        self.assertContains(response, "There are no matching people.")
+        self.assertContains(response, "There are no matching teams.")
 
-    def test_fruits(self):
+    def test_fruits_all(self):
         response = self.client.get("/search/", {"query": "fruit"}, follow=True)
         self.assertEqual(response.status_code, 200)
-        print(response.content)
-        for page in self.content_owner_pages:
-            self.assertContains(response, page.title)
-        for page in self.pages:
-            self.assertContains(response, page.title)
 
-        self.assertContains(response, "All&nbsp;(5)")
-        self.assertContains(response, "People&nbsp;(0)")
-        self.assertContains(response, "Teams&nbsp;(0)")
+        self.assertContains(response, "All&nbsp;(7)")
+        self.assertContains(response, "People&nbsp;(1)")
+        self.assertContains(response, "Teams&nbsp;(1)")
         self.assertContains(response, "Guidance&nbsp;(2)")
         self.assertContains(response, "Tools&nbsp;(1)")
         self.assertContains(response, "News&nbsp;(1)")
+        self.assertContains(response, "5 pages")
+        self.assertContains(response, "1 person")
+        self.assertContains(response, "1 team")
+        self.assertContains(response, "How to eat Fruit")
+        self.assertContains(response, "What fruit can I eat?")
+        self.assertContains(response, "How do I find fruit?")
+        self.assertContains(response, "Fruit finder")
+        self.assertContains(response, "Big fruit news")
+        self.assertNotContains(response, "An irrelevant page")
+        self.assertNotContains(response, "Policies not relating to food")
+        self.assertNotContains(response, "How to write test content")
+        self.assertNotContains(response, "Internal tooling tool")
+        self.assertNotContains(response, "New news")
+        self.assertContains(response, "Martin Fruit")
+        self.assertNotContains(response, "Adam Ant")
+        self.assertContains(response, "Fruit pickers")
+        self.assertNotContains(response, "Middle managers")
+
+    def test_fruits_news(self):
+        response = self.client.get("/search/news/", {"query": "fruit"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "All&nbsp;(7)")
+        self.assertContains(response, "People&nbsp;(1)")
+        self.assertContains(response, "Teams&nbsp;(1)")
+        self.assertContains(response, "Guidance&nbsp;(2)")
+        self.assertContains(response, "Tools&nbsp;(1)")
+        self.assertContains(response, "News&nbsp;(1)")
+        self.assertNotContains(response, "How to eat Fruit")
+        self.assertNotContains(response, "What fruit can I eat?")
+        self.assertNotContains(response, "How do I find fruit?")
+        self.assertNotContains(response, "Fruit finder")
+        self.assertContains(response, "Big fruit news")
+        self.assertNotContains(response, "An irrelevant page")
+        self.assertNotContains(response, "Policies not relating to food")
+        self.assertNotContains(response, "How to write test content")
+        self.assertNotContains(response, "Internal tooling tool")
+        self.assertNotContains(response, "New news")
+        self.assertNotContains(response, "Martin Fruit")
+        self.assertNotContains(response, "Adam Ant")
+        self.assertNotContains(response, "Fruit pickers")
+        self.assertNotContains(response, "Middle managers")
+
+    def test_fruits_tools(self):
+        response = self.client.get("/search/tools/", {"query": "fruit"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "All&nbsp;(7)")
+        self.assertContains(response, "People&nbsp;(1)")
+        self.assertContains(response, "Teams&nbsp;(1)")
+        self.assertContains(response, "Guidance&nbsp;(2)")
+        self.assertContains(response, "Tools&nbsp;(1)")
+        self.assertContains(response, "News&nbsp;(1)")
+        self.assertNotContains(response, "How to eat Fruit")
+        self.assertNotContains(response, "What fruit can I eat?")
+        self.assertNotContains(response, "How do I find fruit?")
+        self.assertContains(response, "Fruit finder")
+        self.assertNotContains(response, "Big fruit news")
+        self.assertNotContains(response, "An irrelevant page")
+        self.assertNotContains(response, "Policies not relating to food")
+        self.assertNotContains(response, "How to write test content")
+        self.assertNotContains(response, "Internal tooling tool")
+        self.assertNotContains(response, "New news")
+        self.assertNotContains(response, "Martin Fruit")
+        self.assertNotContains(response, "Adam Ant")
+        self.assertNotContains(response, "Fruit pickers")
+        self.assertNotContains(response, "Middle managers")
+
+    def test_fruits_guidance(self):
+        response = self.client.get("/search/guidance/", {"query": "fruit"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "All&nbsp;(7)")
+        self.assertContains(response, "People&nbsp;(1)")
+        self.assertContains(response, "Teams&nbsp;(1)")
+        self.assertContains(response, "Guidance&nbsp;(2)")
+        self.assertContains(response, "Tools&nbsp;(1)")
+        self.assertContains(response, "News&nbsp;(1)")
+        self.assertContains(response, "How to eat Fruit")
+        self.assertContains(response, "What fruit can I eat?")
+        self.assertNotContains(response, "How do I find fruit?")
+        self.assertNotContains(response, "Fruit finder")
+        self.assertNotContains(response, "Big fruit news")
+        self.assertNotContains(response, "An irrelevant page")
+        self.assertNotContains(response, "Policies not relating to food")
+        self.assertNotContains(response, "How to write test content")
+        self.assertNotContains(response, "Internal tooling tool")
+        self.assertNotContains(response, "New news")
+        self.assertNotContains(response, "Martin Fruit")
+        self.assertNotContains(response, "Adam Ant")
+        self.assertNotContains(response, "Fruit pickers")
+        self.assertNotContains(response, "Middle managers")
+
+    def test_fruits_teams(self):
+        response = self.client.get("/search/teams/", {"query": "fruit"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "All&nbsp;(7)")
+        self.assertContains(response, "People&nbsp;(1)")
+        self.assertContains(response, "Teams&nbsp;(1)")
+        self.assertContains(response, "Guidance&nbsp;(2)")
+        self.assertContains(response, "Tools&nbsp;(1)")
+        self.assertContains(response, "News&nbsp;(1)")
+        self.assertNotContains(response, "How to eat Fruit")
+        self.assertNotContains(response, "What fruit can I eat?")
+        self.assertNotContains(response, "How do I find fruit?")
+        self.assertNotContains(response, "Fruit finder")
+        self.assertNotContains(response, "Big fruit news")
+        self.assertNotContains(response, "An irrelevant page")
+        self.assertNotContains(response, "Policies not relating to food")
+        self.assertNotContains(response, "How to write test content")
+        self.assertNotContains(response, "Internal tooling tool")
+        self.assertNotContains(response, "New news")
+        self.assertNotContains(response, "Martin Fruit")
+        self.assertNotContains(response, "Adam Ant")
+        self.assertContains(response, "Fruit pickers")
+        self.assertNotContains(response, "Middle managers")
+
+    def test_fruits_people(self):
+        response = self.client.get("/search/people/", {"query": "fruit"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "All&nbsp;(7)")
+        self.assertContains(response, "People&nbsp;(1)")
+        self.assertContains(response, "Teams&nbsp;(1)")
+        self.assertContains(response, "Guidance&nbsp;(2)")
+        self.assertContains(response, "Tools&nbsp;(1)")
+        self.assertContains(response, "News&nbsp;(1)")
+        self.assertNotContains(response, "How to eat Fruit")
+        self.assertNotContains(response, "What fruit can I eat?")
+        self.assertNotContains(response, "How do I find fruit?")
+        self.assertNotContains(response, "Fruit finder")
+        self.assertNotContains(response, "Big fruit news")
+        self.assertNotContains(response, "An irrelevant page")
+        self.assertNotContains(response, "Policies not relating to food")
+        self.assertNotContains(response, "How to write test content")
+        self.assertNotContains(response, "Internal tooling tool")
+        self.assertNotContains(response, "New news")
+        self.assertContains(response, "Martin Fruit")
+        self.assertNotContains(response, "Adam Ant")
+        self.assertNotContains(response, "Fruit pickers")
+        self.assertNotContains(response, "Middle managers")
--- src/peoplefinder/test/factories.py
+++ src/peoplefinder/test/factories.py
@@ -9,11 +9,11 @@ class PersonFactory(factory.django.DjangoModelFactory):
 
 
 class TeamFactory(factory.django.DjangoModelFactory):
-    """Team factory class which defaults to DIT."""
+    """Team factory class which defaults to DBT."""
 
     class Meta:
         model = Team
 
-    name = "Department for International Trade"
-    abbreviation = "DIT"
-    slug = "department-for-international-trade"
+    name = "Department for Business and Trade"
+    abbreviation = "DBT"
+    slug = "department-for-business-and-trade"
