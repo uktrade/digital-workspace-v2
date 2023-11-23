3120e21deef565110678e9b541b4b1d280c53d40
Marcel Kornblum <marcelkornblum@users.noreply.github.com>
2023-11-23 09:49:31 +0000

    Create conftest.py

---
+++ src/extended_search/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from django.core.management import call_command
+
+
+@pytest.fixture(scope="package")
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command("loaddata", "countries.json")
