a74e2b93fa7a6efc6da92825fac505871f3d3763
Marcel Kornblum <marcelkornblum@users.noreply.github.com>
2023-11-23 10:04:08 +0000

    Update conftest.py

--- src/extended_search/tests/conftest.py
+++ src/extended_search/tests/conftest.py
@@ -6,3 +6,4 @@ from django.core.management import call_command
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command("loaddata", "countries.json")
+        call_command("create_section_homepages")
