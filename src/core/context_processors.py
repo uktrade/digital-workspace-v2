77698b2a87c1cce74b769f0696e26a366f183095
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-12-15 12:08:39 +0000

    Hide the problem form for non-authenticated users

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -3,7 +3,8 @@ from core.forms import PageProblemFoundForm
 
 def page_problem_form(request):
     return {
+        "USER_IS_AUTHENTICATED": request.user.is_authenticated,
         "PAGE_PROBLEM_FORM": PageProblemFoundForm(
             initial={"page_url": request.build_absolute_uri()}
-        )
+        ),
     }
