6f2df678cc14a8f458ff51127654f8e0173849c7
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-04-08 09:49:35 +0100

    wip

--- src/intranet_profile/templatetags/bookmarks.py
+++ src/intranet_profile/templatetags/bookmarks.py
@@ -1,6 +1,7 @@
 from django import template
 from django.urls import reverse
 from django.templatetags.static import static
+from wagtail.models import Page
 
 from intranet_profile import is_page_bookmarked
 
@@ -10,6 +11,9 @@ register = template.Library()
 @register.inclusion_tag("intranet_profile/bookmark_page.html")
 def bookmark_page(user, page):
     if page is None:
+        return ""
+
+    if not isinstance(page, Page):
         return ""
 
     is_bookmarked = is_page_bookmarked(user, page)
