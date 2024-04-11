f5e12d3dc2b8a7e8ba3256e879977d7f45c5d78a
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-04-11 14:03:10 +0100

    Be a little more specific

--- src/extended_search/backends/backend.py
+++ src/extended_search/backends/backend.py
@@ -284,9 +284,8 @@ class BoostSearchQueryCompiler(ExtendedSearchQueryCompiler):
         if boost != 1.0:
             if "multi_match" in match_query:
                 match_query["multi_match"]["boost"] = boost
-            else:
+            elif "match_phrase" in match_query:
                 for field in fields:
-                    # TODO: Revisit this....
                     query = match_query["match_phrase"][field.field_name]
                     if isinstance(query, dict) and "boost" in query:
                         match_query["match_phrase"][field.field_name]["boost"] = boost
