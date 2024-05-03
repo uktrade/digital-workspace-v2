24f71262ecd8f19e2be06fa61656d95b3f11ab53
Nico Picchio <70696511+nicopicchio@users.noreply.github.com>
2024-05-03 16:47:09 +0100

    DWPF-1634 New CTA component on wagtail templates (#585)

--- src/content/blocks.py
+++ src/content/blocks.py
@@ -163,6 +163,19 @@ class EmbedVideoBlock(blocks.StructBlock):
         label = "Embed Video"
 
 
+class CTABlock(blocks.StructBlock):
+    """Call to action section"""
+
+    text = blocks.CharBlock(required=True, max_length=40)
+    page = blocks.PageChooserBlock(required=False)
+    url = blocks.URLBlock(required=False)
+
+    class Meta:
+        template = "blocks/cta.html"
+        icon = "thumbtack"
+        label = "CTA Button"
+
+
 class TitleBlock(blocks.CharBlock):
     """A (section) heading"""
 
