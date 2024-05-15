f86d2d93c932b3ea5f80dae722b05e0f19f15773
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-05-15 16:04:16 +0100

    sort imports

--- src/extended_search/tests/test_indexed.py
+++ src/extended_search/tests/test_indexed.py
@@ -6,7 +6,7 @@ from wagtail.models import Page
 from wagtailmedia.models import Media
 
 from about_us.models import AboutUs, AboutUsHome
-from content.models import BasePage, ContentPage, ServiceNavigation, BlogIndex, BlogPost
+from content.models import BasePage, BlogIndex, BlogPost, ContentPage, ServiceNavigation
 from country_fact_sheet.models import CountryFactSheetHome
 from extended_search.index import DWIndexedField, class_is_indexed, get_indexed_models
 from extended_search.management.commands.create_index_fields_json import (
