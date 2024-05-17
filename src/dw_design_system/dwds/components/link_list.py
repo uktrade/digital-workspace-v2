c11999cf7092e9700b0125bab4d96c9f109ec1f8
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-05-17 13:38:57 +0100

    style link list

--- src/dw_design_system/dwds/components/link_list.py
+++ src/dw_design_system/dwds/components/link_list.py
@@ -1,6 +1,35 @@
 from wagtail import blocks
 
 
+class CustomPageLinkListBlock(blocks.StructBlock):
+    class Meta:
+        template = "dwds/components/link_list.html"
+        label = "Link list"
+        help_text = "List of links to specific pages"
+
+    title = blocks.CharBlock(
+        label="Title",
+        icon="title",
+        classname="full title",
+        max_length=40,
+        search_index=False,
+    )
+    description = blocks.CharBlock(required=False, max_length=40)
+    pages = blocks.ListBlock(blocks.PageChooserBlock(label="Page"), search_index=False)
+
+    def get_context(self, value, parent_context=None):
+        context = parent_context or {}
+        context.update(
+            title=value["title"],
+            description=value["description"],
+            list=[
+                {"url": page.get_url(), "text": page.title} for page in value["pages"]
+            ],
+        )
+        return context
+
+
+# Old blocks not to be used for dwds link list component.
 class TitleBlock(blocks.CharBlock):
     """A (section) heading"""
 
@@ -15,25 +44,3 @@ class PagePickerBlock(blocks.PageChooserBlock):
 
     class Meta:
         label = "Link"
-
-
-class CustomPageLinkListBlock(blocks.StructBlock):
-    class Meta:
-        template = "dwds/components/link_list.html"
-        label = "Link list"
-        help_text = "List of links to specific pages"
-
-    title = TitleBlock(search_index=False)
-    description = blocks.CharBlock(required=False, max_length=40)
-    pages = blocks.ListBlock(PagePickerBlock(), search_index=False)
-
-    def get_context(self, value, parent_context=None):
-        context = parent_context or {}
-        context.update(
-            title=value["title"],
-            description=value["description"],
-            list=[
-                {"url": page.get_url(), "text": page.title} for page in value["pages"]
-            ],
-        )
-        return context
