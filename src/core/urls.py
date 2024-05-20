d74c6906422cfaa631f74a0a0c84197f1bc99ee6
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-05-20 16:15:50 +0100

    wip

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,11 +6,11 @@ from core import views
 urlpatterns = [
     path("problem-found/", views.page_problem_found, name="page_problem_found"),
     path("deactivated/", views.deactivated, name="deactivated"),
-    path("report/user-groups", views.user_groups_report, name="user-groups-report"),
+    path("report/user-groups", views.user_groups_report, name="user_groups_report"),
     path(
         "report/content-owners/",
         views.content_owners_report,
         name="report_content_owners",
     ),
-    path("tag/<slug:slug>", views.tag_index, name="tag-index"),
+    path("tag/<slug:slug>", views.tag_index, name="tag_index"),
 ]
