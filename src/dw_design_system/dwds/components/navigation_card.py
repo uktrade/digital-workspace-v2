427a116452d100b3013c3fed963856fd2db629ab
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-05-24 17:33:47 +0100

    makes card title optional and adds linked page title if no title present

--- src/dw_design_system/dwds/components/navigation_card.py
+++ src/dw_design_system/dwds/components/navigation_card.py
@@ -4,8 +4,12 @@ from wagtail import blocks
 class NavigationCardBlock(blocks.StructBlock):
     """A nav card to direct users"""
 
-    title = blocks.CharBlock(required=True, max_length=30)
-    page = blocks.PageChooserBlock(required=True)
+    page = blocks.PageChooserBlock()
+    title = blocks.CharBlock(
+        required=False,
+        max_length=30,
+        help_text="By default, the navigation card component will render the name of the page you linked above. If you have to change this behavior, add an ad hoc title in the box below. Please use sparingly.",
+    )
     summary = blocks.CharBlock(required=False, max_length=70)
 
     class Meta:
@@ -15,9 +19,10 @@ class NavigationCardBlock(blocks.StructBlock):
 
     def get_context(self, value, parent_context=None):
         context = parent_context or {}
+        page = value["page"]
         context.update(
-            title=value["title"],
-            url=value["page"].get_url(),
+            title=value.get("title") or page.title,
+            url=page.get_url(),
             summary=value["summary"],
         )
         return context
