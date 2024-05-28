4f41cf52e806cb0dfbfcee04e4b60f793ae162af
Nico Picchio <70696511+nicopicchio@users.noreply.github.com>
2024-05-28 10:02:07 +0100

    DWPF-1763 Navigation card title made optional (#620)
    Co-authored-by: Marcel Kornblum <marcelkornblum@users.noreply.github.com>

--- src/dw_design_system/dwds/components/navigation_card.py
+++ src/dw_design_system/dwds/components/navigation_card.py
@@ -4,8 +4,12 @@ from wagtail import blocks
 class NavigationCardBlock(blocks.StructBlock):
     """A nav card to direct users"""
 
-    title = blocks.CharBlock(required=True, max_length=30)
-    page = blocks.PageChooserBlock(required=True)
+    page = blocks.PageChooserBlock()
+    title = blocks.CharBlock(
+        required=False,
+        max_length=30,
+        help_text="By default, the name of the page will be the title. Override it using the below field.",
+    )
     summary = blocks.CharBlock(required=False, max_length=70)
 
     class Meta:
@@ -15,9 +19,10 @@ class NavigationCardBlock(blocks.StructBlock):
 
     def get_context(self, value, parent_context=None):
         context = parent_context or {}
+        page = value["page"]
         context.update(
-            title=value["title"],
-            url=value["page"].get_url(),
+            title=value.get("title") or page.title,
+            url=page.get_url(),
             summary=value["summary"],
         )
         return context
