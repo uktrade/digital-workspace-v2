79b63bc68cc20ab618e11569025f9d2b2dd461fe
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-06-05 13:56:15 +0100

    DWPF-1685 Improve page bookmarks (#633)

--- src/interactions/urls.py
+++ src/interactions/urls.py
@@ -7,4 +7,6 @@ app_name = "interactions"
 
 urlpatterns = [
     path("bookmark", views.bookmark, name="bookmark"),
+    path("bookmark/<int:pk>/remove", views.remove_bookmark, name="bookmark-remove"),
+    path("bookmarks", views.bookmark_index, name="bookmark-index"),
 ]
