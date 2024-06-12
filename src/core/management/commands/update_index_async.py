4077357e2779555802d5737f040f6dbaad48f6fb
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-06-12 12:02:15 +0100

    Update the update_index_async command to only run if the index mapping has changed

--- src/core/management/commands/update_index_async.py
+++ src/core/management/commands/update_index_async.py
@@ -1,4 +1,6 @@
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
+from wagtail.search.management.commands.update_index import Command
 
 
 class Command(BaseCommand):
@@ -7,8 +9,41 @@ class Command(BaseCommand):
         " command"
     )
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--force",
+            help="Force the update of the search index",
+            dest="force",
+            default=False,
+        )
+
     def handle(self, *args, **options):
         from core.tasks import update_search_index
+        from extended_search.management.commands.create_index_mapping_json import (
+            get_indexed_mapping_dict,
+        )
+
+        force = options["force"]
+        perform_update = force
+
+        if not force:
+            # Get the current search mapping hash
+            sm_hash = cache.get("search_mapping_hash")
+
+            # Get the search mapping dict
+            sm_dict = get_indexed_mapping_dict()
+            # Get the hash of the current search mapping dict
+            new_sm_hash = hash(frozenset(sm_dict.items()))
+
+            # If the hash of the current search mapping dict is different from the
+            # hash of the search mapping dict that was stored in the cache, then
+            # update the search index.
+            if sm_hash != new_sm_hash:
+                cache.set("search_mapping_hash", new_sm_hash)
+                perform_update = True
+
+        if not perform_update:
+            return
 
         update_search_index.delay()
 
