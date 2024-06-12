9fd62d5945fe77cc31bc14d21152a86d638c06dd
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-06-12 12:35:24 +0100

    Fix sorting of the mapping json so it doesn't change each time

--- src/extended_search/management/commands/create_index_mapping_json.py
+++ src/extended_search/management/commands/create_index_mapping_json.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
 from django.core.management.base import BaseCommand
+from wagtail.documents.models import Document
+from wagtail.images.models import Image
 from wagtail.search.backends import get_search_backend
+from wagtailmedia.models import Media
 
 from extended_search.index import get_indexed_models
 
@@ -10,17 +13,33 @@ from extended_search.index import get_indexed_models
 JSON_FILE = Path(__file__).parent / "indexed_mapping.json"
 
 
+def get_sorted_mapping(search_backend, model):
+    mapping = search_backend.mapping_class(model).get_mapping()
+
+    def sort_dict(d):
+        sorted_dict = dict(sorted(d.items()))
+        for k, v in d.items():
+            if isinstance(v, dict):
+                sorted_dict[k] = sort_dict(v)
+            else:
+                sorted_dict[k] = v
+        return sorted_dict
+
+    return sort_dict(mapping)
+
+
 def get_indexed_mapping_dict():
     """
     Return a dictionary of indexed models and their fields
     Ignoring some models that we don't care about.
     """
-
     search_backend = get_search_backend()
 
     return {
-        str(model): search_backend.mapping_class(model).get_mapping()
+        str(model): get_sorted_mapping(search_backend, model)
         for model in get_indexed_models()
+        # if model not in [Media, Document, Image]
+        if model._meta.app_label != "testapp"
     }
 
 
