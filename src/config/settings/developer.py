dfa8c95e3d67c8be3e7e4b4b206419d8ddba6f3b
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-07-17 14:24:05 +0100

    Local file upload no longer uses AWS/S3

--- src/config/settings/developer.py
+++ src/config/settings/developer.py
@@ -2,15 +2,15 @@ from .base import *  # noqa
 
 CAN_ELEVATE_SSO_USER_PERMISSIONS = True
 
-# Set to True if you need to upload documents and you are not running
-# the ClamAV service locally.
-SKIP_CLAM_AV_FILE_UPLOAD = False
-
-if SKIP_CLAM_AV_FILE_UPLOAD:
-    FILE_UPLOAD_HANDLERS = ("django_chunk_upload_handlers.s3.S3FileUploadHandler",)
-
 INSTALLED_APPS += [  # noqa F405
     "django_extensions",
+]
+
+MEDIA_URL = "/media/"
+STORAGES["default"]["BACKEND"] = "django.core.files.storage.FileSystemStorage"  # noqa F405
+FILE_UPLOAD_HANDLERS = [
+    "django.core.files.uploadhandler.MemoryFileUploadHandler",
+    "django.core.files.uploadhandler.TemporaryFileUploadHandler",
 ]
 
 try:
