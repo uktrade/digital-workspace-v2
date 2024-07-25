14599fef5eb8058023e3d130d136956140065e9a
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-07-25 15:38:37 +0100

    INTR-190 Update engagement card style (#673)

--- src/dw_design_system/dwds/components/engagement_card.py
+++ src/dw_design_system/dwds/components/engagement_card.py
@@ -40,6 +40,7 @@ class EngagementCardBlock(blocks.StructBlock):
             date=page.published_date,
             url=page.url,
             is_highlighted=isinstance(page, NewsPage) or isinstance(page, BlogPost),
+            comment_count=page.get_comment_count(),
         )
 
         return context
