55f632ec0d91228d2419584602febe5bfa408ea4
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-08-02 09:28:42 +0100

    INTR-219 Various styling fixes (#684)
    Co-authored-by: Cameron Lamb <cameron.lamb@digital.trade.gov.uk>

--- src/dw_design_system/urls.py
+++ src/dw_design_system/urls.py
@@ -4,6 +4,7 @@ from dw_design_system import views
 
 
 urlpatterns = [
+    path("styles/", views.styles, name="dwds-styles"),
     path("", views.components, name="dwds-components"),
     path("get/", views.get_component, name="dwds-component-get"),
 ]
