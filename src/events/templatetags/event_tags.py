c554a1c0a68452cad4a52f254d730ff752cde8b4
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-09-02 14:24:07 +0100

    creates date-time string for template

--- src/events/templatetags/event_tags.py
+++ src/events/templatetags/event_tags.py
@@ -1,11 +1,11 @@
 from django import template
 from events.models import EventPage
-from datetime import datetime
 
 register = template.Library()
 
 @register.simple_tag
 def get_event_date(event: EventPage) -> str:
-    # TODO: format string to become a date
-    # event_date = datetime(event.event_date.strftime("%d %b %Y"))
-    return f"{event.event_date}, {event.start_time}-{event.end_time} GMT"
+    event_date = event.event_date.strftime("%-d %B %Y")
+    start_time = event.start_time.strftime("%-I%p").lstrip('0').lower()
+    end_time = event.end_time.strftime("%-I%p").lstrip('0').lower()
+    return f"{event_date}, {start_time}-{end_time} GMT"
