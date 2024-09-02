70ffa37b3135f4f0b3d0dc025c959edf96880e2c
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-09-02 15:06:27 +0100

    moves events layout from stacked to grid

--- src/events/templatetags/event_tags.py
+++ src/events/templatetags/event_tags.py
@@ -3,9 +3,10 @@ from events.models import EventPage
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_event_date(event: EventPage) -> str:
-    event_date = event.event_date.strftime("%-d %B %Y")
-    start_time = event.start_time.strftime("%-I%p").lstrip('0').lower()
-    end_time = event.end_time.strftime("%-I%p").lstrip('0').lower()
-    return f"{event_date}, {start_time}-{end_time} GMT"
+    event_date = event.event_date.strftime("%-d %B")
+    start_time = event.start_time.strftime("%-I%p").lstrip("0").lower()
+    end_time = event.end_time.strftime("%-I%p").lstrip("0").lower()
+    return f"{event_date} {start_time}-{end_time} GMT"
