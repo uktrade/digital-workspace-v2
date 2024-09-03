efaefcdfa737d84ab7e1a77056b132eb234e5aea
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-09-03 11:30:40 +0100

    implements events section in a six card structure

--- src/events/templatetags/event_tags.py
+++ src/events/templatetags/event_tags.py
@@ -1,12 +1,26 @@
+from datetime import time
+
 from django import template
+
 from events.models import EventPage
+
 
 register = template.Library()
 
 
 @register.simple_tag
 def get_event_date(event: EventPage) -> str:
-    event_date = event.event_date.strftime("%-d %B")
-    start_time = event.start_time.strftime("%-I%p").lstrip("0").lower()
-    end_time = event.end_time.strftime("%-I%p").lstrip("0").lower()
-    return f"{event_date} {start_time}-{end_time} GMT"
+    return event.event_date.strftime("%-d %B %Y")
+
+
+@register.simple_tag
+def get_event_time(event: EventPage) -> str:
+    start_time = format_time(event.start_time)
+    end_time = format_time(event.end_time)
+    return f"{start_time} - {end_time}"
+
+
+def format_time(time_obj: time) -> str:
+    if time_obj.minute == 0:
+        return time_obj.strftime("%-I%p").lstrip("0").lower()
+    return time_obj.strftime("%-I:%M%p").lstrip("0").lower()
