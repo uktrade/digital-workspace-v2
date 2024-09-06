86d806a3e786c7bc134947c8489df998cb8d893d
Ryan Williams <ryan.williams2@digital.trade.gov.uk>
2024-09-06 16:10:35 +0100

    changes

--- src/dw_design_system/utils.py
+++ src/dw_design_system/utils.py
@@ -102,11 +102,11 @@ def get_components():
             "name": "Promo Banner",
             "template": "dwds/components/promo.html",
             "context": {
-                "ribbon_text":"One DBT",
-                "description":"We value your ideas to help make it simpler to work at DBT, make a difference and celebrate innovation.",
-                "link_text":"Collaborate and connect",
-                "link_url":"http://localhost:8000/dwds/",
-                "background_image":thumbnail_file
+                "ribbon_text": "One DBT",
+                "description": "We value your ideas to help make it simpler to work at DBT, make a difference and celebrate innovation.",
+                "link_text": "Collaborate and connect",
+                "link_url": "http://localhost:8000/dwds/",
+                "background_image": thumbnail_file,
             },
         },
     ]
