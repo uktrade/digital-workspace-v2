a6de3d4f31c0b3f4d78c73a3b5921f15c0f59226
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-09-09 15:19:22 +0100

    runs linter

--- src/home/models.py
+++ src/home/models.py
@@ -19,6 +19,7 @@ from interactions import get_bookmarks
 from news.models import NewsPage
 from working_at_dit.models import HowDoI
 
+
 # TODO: Add event page here once merged in
 HOME_PRIORITY_PAGE_TYPES = (NewsPage,)
 
