dda0e38418eb11f581e660b9975430491d1ced48
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-09-12 12:23:58 +0100

    Fix home page preview breaking when changing priority pages (#730)
    Co-authored-by: Cameron Lamb <cameron.lamb@digital.trade.gov.uk>

--- src/home/forms.py
+++ src/home/forms.py
@@ -11,7 +11,15 @@ class HomePageForm(WagtailAdminPageForm):
         )
 
         page_count_for_layout = sum(priority_page_layout.to_page_counts())
-        priority_pages_count = len(self.formsets["priority_pages"].cleaned_data)
+
+        # Filter out the forms which have been deleted. I found the approach here
+        # https://stackoverflow.com/a/71871397.
+        priority_pages_forms = [
+            form
+            for form in self.formsets["priority_pages"]
+            if form not in self.formsets["priority_pages"].deleted_forms
+        ]
+        priority_pages_count = len(priority_pages_forms)
 
         if priority_pages_count < page_count_for_layout:
             raise ValidationError(
