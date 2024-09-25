e679646d513f09cfb3275f88a72b19e4d8dca911
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2024-09-25 11:49:49 +0100

    update page_url field for hrfeedback

--- src/core/models/fields.py
+++ src/core/models/fields.py
@@ -6,7 +6,6 @@ class URLField(models.URLField):
         kwargs.setdefault("max_length", 2048)
         super().__init__(**kwargs)
 
-
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
         if kwargs.get("max_length") == 2048:
