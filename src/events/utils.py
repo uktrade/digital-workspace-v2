fdea95258b74b8526c63ff5b12e7f51280d7b6e5
David Okeke <david.okeke@madetech.com>
2024-10-07 15:47:32 +0100

    Changed safe to mark_safe function

--- src/events/utils.py
+++ src/events/utils.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING
 
+from django.utils.safestring import mark_safe
+
 from core.utils import format_time
 
 
@@ -14,10 +16,12 @@ def get_event_datetime_display_string(event: "EventPage") -> str:
     event_end_time = format_time(event.event_end.time())
 
     if event_start_date == event_end_date:
-        event_post_title = (
+        event_post_title = mark_safe(  # noqa S308
             f"{event_start_date} <br>{event_start_time} - {event_end_time}"
         )
     else:
-        event_post_title = f"{event_start_date} {event_start_time} - <br>{event_end_date} {event_end_time}"
+        event_post_title = mark_safe(  # noqa S308
+            f"{event_start_date} {event_start_time} - <br>{event_end_date} {event_end_time}"
+        )
 
     return event_post_title
