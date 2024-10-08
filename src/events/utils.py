d6a701f5634611fbc866db3f502d511eebc3b89b
David Okeke <140066072+david-okeke1337@users.noreply.github.com>
2024-10-08 17:37:39 +0100

    INTR-342 Event page start/end dates (#766)
    Co-authored-by: Cameron Lamb <cameron.lamb@digital.trade.gov.uk>

--- src/events/utils.py
+++ src/events/utils.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING
 
+from django.utils.safestring import mark_safe
+
 from core.utils import format_time
 
 
@@ -7,11 +9,19 @@ if TYPE_CHECKING:
     from events.models import EventPage
 
 
-def get_event_date(event: "EventPage") -> str:
-    return event.event_date.strftime("%-d %B %Y")
+def get_event_datetime_display_string(event: "EventPage") -> str:
+    event_start_date = event.event_start.strftime("%-d %B %Y")
+    event_end_date = event.event_end.strftime("%-d %B %Y")
+    event_start_time = format_time(event.event_start.time())
+    event_end_time = format_time(event.event_end.time())
 
+    if event_start_date == event_end_date:
+        event_post_title = mark_safe(  # noqa S308
+            f"{event_start_date} <br>{event_start_time} - {event_end_time}"
+        )
+    else:
+        event_post_title = mark_safe(  # noqa S308
+            f"{event_start_date} {event_start_time} - <br>{event_end_date} {event_end_time}"
+        )
 
-def get_event_time(event: "EventPage") -> str:
-    start_time = format_time(event.start_time)
-    end_time = format_time(event.end_time)
-    return f"{start_time} - {end_time}"
+    return event_post_title
