3734a3df1742a50a8480ebc270d73ce2a85e8c36
Marcel Kornblum <marcelkornblum@users.noreply.github.com>
2024-11-08 09:32:30 +0000

    anon-events

--- src/events/views.py
+++ src/events/views.py
@@ -39,22 +39,23 @@ def to_ical(event: EventPage) -> Event:
     ical_event = Event()
 
     ical_event["uid"] = event.pk
-    ical_event.add("summary", event.title)
+    # ical_event.add("summary", event.title)
+    ical_event.add("summary", "Anonymous event title")
     ical_event.add("dtstart", event.event_start)
     ical_event.add("dtend", event.event_end)
     ical_event.add("dtstamp", event.last_published_at)
 
     ical_event.add(
-        "description", f"<a href='{event.url}'>For details see event listing</a>"
+        "description", f"See event listing: {event.url}"
     )
-    if event.location:
-        ical_event["location"] = event.location.name
+    # if event.location:
+    #     ical_event["location"] = event.location.name
     ical_event.add("priority", 5)
 
-    author = event.get_first_publisher()
-    organiser = vCalAddress(f"MAILTO:{author.profile.preferred_email}")
-    organiser.params["cn"] = vText(author.profile.full_name)
-    ical_event["organizer"] = organiser
+    # author = event.get_first_publisher()
+    # organiser = vCalAddress(f"MAILTO:{author.profile.preferred_email}")
+    # organiser.params["cn"] = vText(author.profile.full_name)
+    # ical_event["organizer"] = organiser
 
     return ical_event
 
