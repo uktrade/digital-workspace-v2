f3e8d9daa996a36c01cf7bd9ca93780e72075025
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2024-11-20 17:19:20 +0000

    Simplified js code

--- src/dw_design_system/dwds/components/accordion.js
+++ src/dw_design_system/dwds/components/accordion.js
@@ -1,66 +1,68 @@
-function toggleSpecificSection(element, headerElement, chevronElement) {
-    if (element.style.display === "none") {
-        element.style.display = "block";
-        headerElement.innerText = "Hide";
-        chevronElement.classList.remove("dwds-accordion-nav-chevron-down");
+function showSection(accordion) {
+    const content = accordion.getElementsByClassName("dwds-accordion-section-content")[0];
+    const text = accordion.getElementsByClassName("dwds-accordion-section-toggle-text")[0];
+    const sectionChevron = accordion.getElementsByClassName("dwds-accordion-section-chevron")[0];
+    content.style.display = "block";
+    text.innerText = "Hide";
+    sectionChevron.classList.remove("dwds-accordion-nav-chevron-down");
+}
+
+function hideSection(accordion) {
+    const content = accordion.getElementsByClassName("dwds-accordion-section-content")[0];
+    const text = accordion.getElementsByClassName("dwds-accordion-section-toggle-text")[0];
+    const sectionChevron = accordion.getElementsByClassName("dwds-accordion-section-chevron")[0];
+    content.style.display = "none";
+    text.innerText = "Show";
+    sectionChevron.classList.add("dwds-accordion-nav-chevron-down");
+}
+
+function toggleSpecificSection(element) {
+    const accordionContent = element.getElementsByClassName("dwds-accordion-section-content")[0];
+    if (accordionContent.style.display === "none") {
+        showSection(element);
     } else {
-        element.style.display = "none";
-        headerElement.innerText = "Show";
-        chevronElement.classList.add("dwds-accordion-nav-chevron-down");
+        hideSection(element);
     }
 }
 
-function toggleAllSections(element) {
-    const contentElements = document.getElementsByClassName("dwds-accordion-section-content");
-    const textElements = document.querySelectorAll(".dwds-accordion-section-toggle-text")
-    var displayStyle = "block";
-    const chevronElements = document.querySelectorAll(".dwds-accordion-section-chevron");
-    const accordionChevronAll = document.getElementsByClassName("dwds-accordion-show-all-chevron")[0];
+function toggleAll(element, allChevron, accordionsInGroup) {
     if (element.innerText === "Hide all sections") {
-        chevronElements.forEach(function(changeArrow){
-            changeArrow.classList.add("dwds-accordion-nav-chevron-down");
+        accordionsInGroup.forEach(function (accordion) {
+            hideSection(accordion);
         });
-        displayStyle = "none";
-        textElements.forEach(function(showEl){
-            showEl.innerText = "Show";
-        });
-        accordionChevronAll.classList.add("dwds-accordion-show-all-chevron-down");
         element.innerText = "Show all sections";
+        allChevron.classList.add("dwds-accordion-show-all-chevron-down");
     } else {
-        accordionChevronAll.classList.remove("dwds-accordion-show-all-chevron-down");
         element.innerText = "Hide all sections";
-        textElements.forEach(function(showEl){
-            showEl.innerText = "Hide";
+        accordionsInGroup.forEach(function (accordion) {
+            showSection(accordion);
         });
-        chevronElements.forEach(function(changeArrow){
-            changeArrow.classList.remove("dwds-accordion-nav-chevron-down");
-        });
-    }
-    for (let i = 0; i < contentElements.length; i++) {
-        contentElements[i].style.display = displayStyle;
+        allChevron.classList.remove("dwds-accordion-show-all-chevron-down");
     }
 }
-
 
 document.addEventListener('DOMContentLoaded', function () {
+
     document.querySelectorAll('.dwds-accordions').forEach(function (accordions) {
-        const accordionsInGroup = accordions.querySelectorAll('.dwds-accordion-section')
+
+        const accordionsInGroup = accordions.querySelectorAll('.dwds-accordion-section');
         accordionsInGroup.forEach(function (accordion) {
             const accordionHeader = accordion.getElementsByClassName("dwds-accordion-section-header")[0];
+            accordionHeader.addEventListener("click", function (e) {
+                toggleSpecificSection(accordion);
+            });
+
             const accordionContent = accordion.getElementsByClassName("dwds-accordion-section-content")[0];
             accordionContent.style.display = "none";
-            const toggleText = accordion.getElementsByClassName("dwds-accordion-section-toggle-text")[0];
             const toggleChevron = accordion.querySelector(".dwds-accordion-section-chevron");
             toggleChevron.classList.add("dwds-accordion-nav-chevron-down");
-            accordionHeader.addEventListener("click", function (e) {
-                toggleSpecificSection(accordionContent, toggleText, toggleChevron);
-            });
         });
+
         const accordionToggleAll = accordions.getElementsByClassName("dwds-accordion-show-all-text")[0];
-        const accordionChevronAll = document.getElementsByClassName("dwds-accordion-show-all-chevron")[0];
-        accordionChevronAll.classList.add("dwds-accordion-show-all-chevron-down");
+        const allChevron = accordions.querySelector(".dwds-accordion-show-all-chevron");
+        allChevron.classList.add("dwds-accordion-show-all-chevron-down");
         accordionToggleAll.addEventListener("click", function (e) {
-            toggleAllSections(accordionToggleAll);
+            toggleAll(accordionToggleAll, allChevron, accordionsInGroup);
         });
     });
 });
