b5c7a15453eb6e35cd93c061c0d2f0dad598b685
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-11-26 15:12:50 +0000

    pagination and show all when there's no query

--- src/peoplefinder/views/directory.py
+++ src/peoplefinder/views/directory.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from django.conf import settings
 from django.db.models.query import QuerySet
 from django.views.generic import ListView
 
@@ -10,13 +11,22 @@ from search.search import PeopleSearchVector
 class PeopleDirectory(ListView):
     model = Person
     template_name = "peoplefinder/person-directory.html"
-    paginate_by = 60
+    paginate_by = 30
 
     def get_queryset(self) -> QuerySet[Any]:
-        people_results = PeopleSearchVector(self.request).search(
-            query_str=self.request.GET.get("query", "")
-        )
-        return people_results
+        query = self.request.GET.get("query", "")
+        if query:
+            people_results = PeopleSearchVector(self.request).search(
+                query_str=self.request.GET.get("query", "")
+            )
+            return people_results
+
+        queryset = super().get_queryset()
+
+        if not self.request.user.has_perm("peoplefinder.can_view_inactive_profiles"):
+            days = settings.SEARCH_SHOW_INACTIVE_PROFILES_WITHIN_DAYS
+            queryset = queryset.active_or_inactive_within(days=days)
+        return queryset
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
