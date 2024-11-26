85bf8f88e7904f727827a96b0a4590b937f320d9
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-11-26 17:23:31 +0000

    Add team people directory filter

--- src/peoplefinder/views/directory.py
+++ src/peoplefinder/views/directory.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.db.models.query import QuerySet
 from django.views.generic import ListView
 
-from peoplefinder.models import Person
+from peoplefinder.models import Person, Team
 from search.search import PeopleSearchVector
 
 
@@ -13,27 +13,52 @@ class PeopleDirectory(ListView):
     template_name = "peoplefinder/person-directory.html"
     paginate_by = 30
 
+    def dispatch(self, request, *args, **kwargs):
+        self.query = self.request.GET.get("query", "")
+
+        self.team = None
+        if team_pk := self.request.GET.get("team", ""):
+            self.team = Team.objects.get(pk=team_pk)
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get_queryset(self) -> QuerySet[Any]:
-        query = self.request.GET.get("query", "")
-        if query:
+        if self.query:
             people_results = PeopleSearchVector(self.request).search(
-                query_str=self.request.GET.get("query", "")
+                query_str=self.query
             )
-            return people_results
+            person_ids = [p.id for p in people_results]
+            queryset = Person.objects.filter(id__in=person_ids)
+        else:
+            queryset = super().get_queryset()
+            if not self.request.user.has_perm(
+                "peoplefinder.can_view_inactive_profiles"
+            ):
+                days = settings.SEARCH_SHOW_INACTIVE_PROFILES_WITHIN_DAYS
+                queryset = queryset.active_or_inactive_within(days=days)
 
-        queryset = super().get_queryset()
+        if self.team:
+            queryset = queryset.filter(
+                roles__team__pk__in=[self.team.pk]
+                + [tt.child.pk for tt in self.team.parents.all()]
+            )
+            print(self.team.parents.all())
+            print(self.team.children.all())
 
-        if not self.request.user.has_perm("peoplefinder.can_view_inactive_profiles"):
-            days = settings.SEARCH_SHOW_INACTIVE_PROFILES_WITHIN_DAYS
-            queryset = queryset.active_or_inactive_within(days=days)
         return queryset
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
+        page_title = "Find people"
+
+        if self.team:
+            page_title = f"Find people in {self.team}"
+
         context.update(
-            page_title="Find people",
+            page_title=page_title,
+            team=self.team,
+            team_breadcrumbs=True,
             extra_breadcrumbs=[
-                ("/", "Home"),
                 (None, "People directory"),
             ],
             search_query=self.request.GET.get("query", ""),
