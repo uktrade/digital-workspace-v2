8e9ae78f8952cb6a69a0e77103584e24226b58d7
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-11-28 12:51:52 +0000

    adds filter to template

--- src/peoplefinder/templatetags/email_word_wrap.py
+++ src/peoplefinder/templatetags/email_word_wrap.py
@@ -4,6 +4,13 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
-def email_word_wrap() -> str:
-    ...
+@register.filter
+def email_word_wrap(email: str) -> str:
+    print(f"Filter called with: {email}")
+    if not isinstance(email, str):
+        return email
+
+    email = email.replace("@", "<wbr>@")
+    email = email.replace(".gov.uk", "<wbr>.gov.uk")
+
+    return email
