75481f9dcd9a1339a2fdb3fd1dce23d13ca67ff1
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2024-11-28 14:49:34 +0000

    adds further word break

--- src/peoplefinder/templatetags/email_word_wrap.py
+++ src/peoplefinder/templatetags/email_word_wrap.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
@@ -6,11 +8,14 @@ register = template.Library()
 
 @register.filter
 def email_word_wrap(email: str) -> str:
-    print(f"Filter called with: {email}")
     if not isinstance(email, str):
         return email
 
+    email = escape(email)
+    if "." in email:
+        name_parts = email.split(".", 1)
+        email = f"{name_parts[0]}<wbr>.{name_parts[1]}"
     email = email.replace("@", "<wbr>@")
     email = email.replace(".gov.uk", "<wbr>.gov.uk")
 
-    return email
+    return mark_safe(email)  # noqa S308
