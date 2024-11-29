96131d06148e3c23baa4d6dea8c62a6accc998de
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2024-11-29 16:05:25 +0000

    Applied make fix and disabled debug flag to false

--- src/manage.py
+++ src/manage.py
@@ -7,7 +7,9 @@ def initialize_debugpy():
     try:
         import debugpy
     except ImportError:
-        sys.stdout.write("debugpy is not installed, please install it with: pip install debugpy\n")
+        sys.stdout.write(
+            "debugpy is not installed, please install it with: pip install debugpy\n"
+        )
         return
 
     if not os.getenv("RUN_MAIN"):
