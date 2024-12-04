2900d48b20f21237fd127f098e836a8bdd0020b3
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-12-04 14:55:20 +0000

    Add Django Debug Toolbar (#825)

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,8 +7,16 @@ require.context("govuk-frontend/govuk/assets");
 require.context(
     "@ministryofjustice/frontend/moj/assets"
 );
-import {initAll} from "govuk-frontend";
+import { initAll } from "govuk-frontend";
 import mojFrontend from "@ministryofjustice/frontend";
 
 initAll();
 mojFrontend.initAll();
+
+if (typeof window.djdt !== "undefined" && typeof window.htmx !== "undefined") {
+    htmx.on("htmx:afterSettle", function (detail) {
+        if (detail.target instanceof HTMLBodyElement) {
+            djdt.show_toolbar();
+        }
+    });
+}
