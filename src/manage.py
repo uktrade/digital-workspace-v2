2b68bb771d20d19d4d1082b0beb2885236529433
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-12-06 11:12:31 +0000

    Add part of the function back

--- src/manage.py
+++ src/manage.py
@@ -12,17 +12,17 @@ def initialize_debugpy():
         )
         return
 
-    if not os.getenv("RUN_MAIN"):
-        debugpy.listen(("0.0.0.0", 5678))
-        sys.stdout.write("debugpy listening on port 5678...\n")
+    # if not os.getenv("RUN_MAIN"):
+    #     debugpy.listen(("0.0.0.0", 5678))
+    #     sys.stdout.write("debugpy listening on port 5678...\n")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 
     from django.conf import settings
 
-    # if settings.DEBUG and settings.ENABLE_DEBUGPY:
-    #     initialize_debugpy()
+    if settings.DEBUG and settings.ENABLE_DEBUGPY:
+        initialize_debugpy()
 
     from django.core.management import execute_from_command_line
 
