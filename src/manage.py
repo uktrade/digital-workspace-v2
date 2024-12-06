ef64b7256a215474144c2fd9d1dc6fb9f7e783e6
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-12-06 11:27:42 +0000

    Increase the exception scope

--- src/manage.py
+++ src/manage.py
@@ -11,6 +11,8 @@ def initialize_debugpy():
             "debugpy is not installed, please install it with: pip install debugpy\n"
         )
         return
+    except Exception:
+        return
 
     # if not os.getenv("RUN_MAIN"):
     #     debugpy.listen(("0.0.0.0", 5678))
