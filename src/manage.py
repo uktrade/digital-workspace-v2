fa600673e9be9291e80db0835f9c5fb62a1d1071
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-12-06 13:39:58 +0000

    Update management command to meet DJango's latest template

--- src/manage.py
+++ src/manage.py
@@ -20,15 +20,12 @@ def main():
 
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == "__main__":
