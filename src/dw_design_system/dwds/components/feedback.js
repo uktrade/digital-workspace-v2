86d8b730d2c46b5f497154f73937a06959da0d59
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2024-12-10 14:18:07 +0000

    Removed fb element open set to true

--- src/dw_design_system/dwds/components/feedback.js
+++ src/dw_design_system/dwds/components/feedback.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const fbElement = document.querySelector('.feedback-section details');
     fbButton.addEventListener('click', function (event) {
         event.preventDefault();
-        fbElement.open = true;
         fbElement.querySelector('*').focus();
     });
 });
