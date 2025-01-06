116774bb4b24f65f6f3c70594251931d708ba616
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2025-01-06 17:55:04 +0000

    INTR:471 Move bookmark to primary page actions (#850)
    Co-authored-by: Cameron Lamb <cameron.lamb@digital.trade.gov.uk>

--- src/interactions/templatetags/bookmarks.py
+++ src/interactions/templatetags/bookmarks.py
@@ -1,19 +1,28 @@
 from django import template
 from django.urls import reverse
+from waffle import flag_is_active
 from wagtail.models import Page
 
+from home.models import HomePage
 from interactions.services import bookmarks as bookmarks_service
 
 
 register = template.Library()
 
 
+# TODO: Remove `bookmark_page_input` once `new_sidebar` flag is removed
 @register.inclusion_tag("interactions/bookmark_page_input.html")
-def bookmark_page_input(user, page):
+def bookmark_page_input(user, page, request):
+    if flag_is_active(request, "new_sidebar"):
+        return {}
+
     if page is None:
         return {}
 
     if not isinstance(page, Page):
+        return {}
+
+    if isinstance(page, HomePage):
         return {}
 
     is_bookmarked = bookmarks_service.is_page_bookmarked(user, page)
