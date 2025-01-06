318f0912d62573aedac6af4bb936831872f9b965
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-01-06 18:09:51 +0000

    Hide give feedback functionality on new sidebar (#852)

--- src/core/templatetags/sidebar.py
+++ src/core/templatetags/sidebar.py
@@ -108,6 +108,9 @@ class GiveFeedback(SidebarPart):
     description = "Did you find what you were looking for?"
 
     def is_visible(self):
+        request = self.context["request"]
+        if not flag_is_active(request, "new_sidebar"):
+            return False
         page = self.context.get("self")
         if isinstance(page, HomePage):
             return False
