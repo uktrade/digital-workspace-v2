73a177afe69562784df64331531e13eebd4fd14a
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-01-10 15:20:16 +0000

    Fix the convert logic after thorough testing

--- src/networks/utils.py
+++ src/networks/utils.py
@@ -2,118 +2,166 @@ import inspect
 from typing import Type
 
 from django.contrib.contenttypes.models import ContentType
-from django.db import connections, transaction
-from wagtail.models import Page
+from django.db import connections, models, transaction
 
 from networks.models import Network, NetworkContentPage, NetworksHome
 
 
-def page_conversion_compatible(
-    from_page_type: Type[Page], to_page_type: Type[Page], allow_one_way: bool = False
-) -> bool:
-    """
-    Check if a page can be converted to another page type.
+def find_first_common_ancestor(
+    from_model: Type[models.Model],
+    to_model: Type[models.Model],
+) -> Type[models.Model]:
+    if from_model == to_model:
+        return from_model
 
-    If `allow_one_way` is True, then the conversion is allowed if the MRO of the
-    "to" page type is a subset of the MRO of the "from" page type. This means
-    that data might be lost in the conversion.
-    """
-    # Check that the MROs are compatible (skip the first element as it is the
-    # page type itself)
-    from_page_mro = inspect.getmro(from_page_type)[1:]
-    to_page_mro = inspect.getmro(to_page_type)[1:]
+    from_mro = inspect.getmro(from_model)
+    to_mro = inspect.getmro(to_model)
 
-    if allow_one_way:
-        return set(to_page_mro).issubset(set(from_page_mro))
-    return from_page_mro == to_page_mro
+    for from_cls in from_mro:
+        if from_cls in to_mro and not from_cls._meta.abstract:
+            return from_cls
+
+    raise ValueError("No common ancestor found")
+
+
+def get_next_model_from_mro(
+    model: Type[models.Model],
+) -> Type[models.Model]:
+    for cls in inspect.getmro(model):
+        if cls == model:
+            continue
+        if not cls._meta.abstract:
+            return cls
+    raise ValueError("No concrete class found")
 
 
 @transaction.atomic
-def convert_page(
-    page: Page, page_type: Type[Page], allow_one_way: bool = False
-) -> NetworkContentPage:
-    """
-    Convert a page to a given page type.
-    """
-    page = page.specific
-
-    if not page_conversion_compatible(
-        from_page_type=page.__class__,
-        to_page_type=page_type,
-        allow_one_way=allow_one_way,
-    ):
-        raise ValueError(
-            f"Page type {page.__class__} cannot be converted to {page_type}"
-        )
-
+def convert(
+    model_instance: models.Model,
+    to_model: Type[models.Model],
+    default_data: dict | None = None,
+):
     db_connection = connections["default"]
 
-    new_content_fields: list[str] = []
-    # Get content type specific fields from the table.
-    with db_connection.cursor() as cursor:
-        cursor.execute(
-            """
-            SELECT column_name
-            FROM information_schema.columns
-            WHERE table_name = %s;
-            """,
-            [page_type._meta.db_table],
+    if default_data is None:
+        default_data = {}
+
+    common_ancestor = find_first_common_ancestor(model_instance.__class__, to_model)
+
+    # Clear all tables up to the common ancestor
+    for cls in inspect.getmro(model_instance.__class__):
+        if cls == common_ancestor:
+            break
+        if cls._meta.abstract:
+            continue
+        next_mro_class = get_next_model_from_mro(cls)
+        ptr_field_name = f"{next_mro_class._meta.model_name}_ptr_id"
+        # Clear the row from the `networks_network` table
+        with db_connection.cursor() as cursor:
+            cursor.execute(
+                f"""
+                DELETE FROM {cls._meta.db_table}
+                WHERE {ptr_field_name}={f"'{model_instance.pk}'"};
+                """,
+            )
+
+    # Create all of the tables down to the target model
+    to_model_mro = inspect.getmro(to_model)
+    # Remove everything in the MRO after, and including, the common ancestor
+    to_model_mro = to_model_mro[: to_model_mro.index(common_ancestor)]
+    # Reverse the MRO so we create the tables in the correct order
+    to_model_mro = to_model_mro[::-1]
+
+    # Update the `content_type` value
+    common_ancestor_instance = common_ancestor.objects.get(pk=model_instance.pk)
+    common_ancestor_instance.content_type = ContentType.objects.get_for_model(to_model)
+    common_ancestor_instance.save(update_fields=["content_type_id"])
+
+    previous_mro_class = common_ancestor
+    for cls in to_model_mro:
+        # Skip abstract classes
+        if cls._meta.abstract:
+            continue
+
+        new_cls_fields = [f.name for f in cls._meta.get_fields()]
+        new_cls_kwargs = model_instance.__dict__.copy()
+
+        # Get a list of fields that are not in the new class
+        fields_to_remove = set(new_cls_kwargs.keys()) - set(new_cls_fields)
+        # Clear the base Model fields.
+        if issubclass(cls, common_ancestor):
+            fields_to_remove.update(
+                set(
+                    [
+                        f.name
+                        for f in common_ancestor._meta.get_fields()
+                        if f.name in new_cls_kwargs
+                    ]
+                )
+            )
+
+        for field in fields_to_remove:
+            del new_cls_kwargs[field]
+
+        new_cls_kwargs[f"{previous_mro_class._meta.model_name}_ptr_id"] = (
+            model_instance.id
         )
-        for row in cursor.fetchall():
-            new_content_fields.append(row[0])
 
-    print(new_content_fields)
+        if cls in default_data:
+            new_cls_kwargs.update(default_data[cls])
 
-    # Add the row to the new table.
-    with db_connection.cursor() as cursor:
-        table_name = page_type._meta.db_table
-        fields = ",".join(new_content_fields)
-        values = ",".join(
-            [f"'{str(getattr(page, field))}'" for field in new_content_fields]
-        )
-        cursor.execute(
-            f"""
-            INSERT INTO {table_name} ({fields})
-            VALUES ({values});
-            """
-        )
+        # Add the row to the new table.
+        with db_connection.cursor() as cursor:
+            table_name = cls._meta.db_table
+            fields = ",".join(new_cls_kwargs.keys())
+            values = ",".join([f"'{str(v)}'" for v in new_cls_kwargs.values()])
+            cursor.execute(
+                f"""
+                INSERT INTO {table_name} ({fields})
+                VALUES ({values});
+                """
+            )
 
-    # Get the content type
-    app_label = page_type._meta.app_label
-    model_name = page_type._meta.model_name
-    content_type = ContentType.objects.get(app_label=app_label, model=model_name)
+        previous_mro_class = cls
 
-    # Update the `wagtailcore_page` table to change the `content_type_id`
-    wagtail_page = Page.objects.get(pk=page.pk)
-    wagtail_page.content_type = content_type
-    wagtail_page.save(update_fields=["content_type_id"])
-
-    # Clear the row from the `networks_network` table
-    with db_connection.cursor() as cursor:
-        cursor.execute(
-            f"""
-            DELETE FROM {page.__class__._meta.db_table} WHERE contentpage_ptr_id=%s;
-            """,
-            [str(page.contentpage_ptr_id)],
-        )
+        if cls == to_model:
+            break
 
 
 def convert_network_to_content_page(page: Network):
     """
     Convert a network page to a content page.
     """
-    convert_page(page=page, page_type=NetworkContentPage)
+    convert(
+        model_instance=page,
+        to_model=NetworkContentPage,
+        default_data={
+            NetworkContentPage: {
+                "content_owner_id": page.content_owner_id,
+                "content_contact_email": page.content_contact_email,
+            }
+        },
+    )
 
 
 def convert_network_content_page_to_network(page: NetworkContentPage):
     """
     Convert a network page to a content page.
     """
-    convert_page(page=page, page_type=Network)
+    convert(
+        model_instance=page,
+        to_model=Network,
+        default_data={
+            Network: {
+                "content_owner_id": page.content_owner_id,
+                "content_contact_email": page.content_contact_email,
+            }
+        },
+    )
 
 
 def convert_network_to_networks_home(page: Network):
     """
     Convert a network page to a networks home page.
     """
-    convert_page(page=page, page_type=NetworksHome, allow_one_way=True)
+    convert(model_instance=page, to_model=NetworksHome)
