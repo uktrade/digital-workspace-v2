7e74f3b010531263210cf4aeac2fc787ba02fab3
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-01-13 16:01:08 +0000

    Update strings

--- src/networks/wagtail_hooks.py
+++ src/networks/wagtail_hooks.py
@@ -9,12 +9,12 @@ from networks.models import Network, NetworkContentPage
 def page_listing_more_buttons(page, page_perms, is_parent=False):
     if isinstance(page, Network):
         yield wagtailadmin_widgets.PageListingButton(
-            "Convert to NetworkContentPage",
+            "Convert to Network Content Page",
             reverse("networks:convert-network-to-content-page", kwargs={"pk": page.pk}),
             priority=10,
         )
         yield wagtailadmin_widgets.PageListingButton(
-            "Convert to NetworksHome",
+            "Convert to Networks Home",
             reverse(
                 "networks:convert-network-to-networks-home", kwargs={"pk": page.pk}
             ),
