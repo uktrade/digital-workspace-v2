daf6d5caa583ab24b8fe52208770f1dea9c3bfef
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2025-01-15 15:42:13 +0000

    Use Page instead BasePage

--- src/interactions/services/reactions.py
+++ src/interactions/services/reactions.py
@@ -1,10 +1,11 @@
-from content.models import BasePage
+from wagtail.models import Page
+
 from interactions.models import Reaction, ReactionType
 from news.models import NewsPage
 from user.models import User
 
 
-def manage_reaction(user: User, page: BasePage, reaction_type: str):
+def manage_reaction(user: User, page: Page, reaction_type: str):
 
     if not isinstance(page, NewsPage):
         raise ValueError("The page must be a NewsPage.")
