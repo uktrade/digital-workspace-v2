6dc8a0a6b8686ed49f85e4435bf96877b9f705c1
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2025-01-21 13:09:18 +0000

    Reorder initialising the default values for reactions count

--- src/interactions/services/reactions.py
+++ src/interactions/services/reactions.py
@@ -35,12 +35,12 @@ def get_reaction_counts(page: Page):
     if not isinstance(page, NewsPage):
         return {}
 
+    reaction_counts = {reaction_type: 0 for reaction_type in ReactionType.values}
+
     reactions = (
         Reaction.objects.filter(page=page).values("type").annotate(count=Count("id"))
     )
-    reaction_counts = {reaction["type"]: reaction["count"] for reaction in reactions}
-
-    for reaction_type in ReactionType.values:
-        reaction_counts.setdefault(reaction_type, 0)
-
+    reaction_counts.update(
+        {reaction["type"]: reaction["count"] for reaction in reactions}
+    )
     return reaction_counts
