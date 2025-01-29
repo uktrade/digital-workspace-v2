cf8d1b83bd04ab2ff74a4de18575e0b9627a1071
David Okeke <david.okeke@madetech.com>
2025-01-29 14:06:03 +0000

    Addressed PR comments

--- src/dw_design_system/dwds/components/copy_text.js
+++ src/dw_design_system/dwds/components/copy_text.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('.dwds-copy-text').forEach(function (copy_text_element) {
-        const inputField = copy_text_element.querySelector(".copy-text-input");
-        const copyButton = copy_text_element.querySelector(".copy-text-button");
+        const inputField = copy_text_element.querySelector("input");
+        const copyButton = copy_text_element.querySelector("button");
 
         copyButton.addEventListener("click", function () {
             const text = inputField.value;
             
             navigator.clipboard.writeText(text);
             inputField.value = "Copied to clipboard";
-            // TODO: Change the copy icon on click?
+            // TODO: INTR-542 - Change the success icon on click
             
             setTimeout(() => (
                 inputField.value = text
