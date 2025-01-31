5174452732a2bcac5bb6bfe025b3c648f8f349ef
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-01-31 11:32:36 +0000

    Clean up `.env` file usage (#899)

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -22,9 +22,6 @@ PROJECT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 
 # Read environment variables using `django-environ`, use `.env` if it exists
 env = environ.Env()
-env_file = os.path.join(PROJECT_ROOT_DIR, ".env")
-if os.path.exists(env_file):
-    env.read_env(env_file)
 env.read_env()
 
 # Set required configuration from environment
