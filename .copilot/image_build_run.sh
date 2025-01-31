5048a5f2ac18ad384087a92187e2695ca1deb0ac
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-01-31 12:38:17 +0000

    Move `.env` loading into build settings

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,20 +2,15 @@
 
 # Exit early if something goes wrong
 set -e
-export DJANGO_SETTINGS_MODULE=config.settings.build
 
 echo "Running post build script"
 
 echo "Renaming .env.ci to .env"
 mv ".env.ci" ".env"
 
-echo "Loading .env"
-set -a
-source .env
-set +a
-
 cd src
 
+export DJANGO_SETTINGS_MODULE=config.settings.build
 echo "Running collectstatic"
 python manage.py collectstatic --noinput
 
--- src/config/settings/build.py
+++ src/config/settings/build.py
@@ -1,3 +1,17 @@
+import os
+
+import environ
+
+
+# Set directories to be used across settings
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+PROJECT_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+# Read environment variables using `django-environ`, use `.env` if it exists
+env = environ.Env()
+env_file = os.path.join(PROJECT_ROOT_DIR, ".env")
+if os.path.exists(env_file):
+    env.read_env(env_file)
+
 from .base import *  # noqa
 
 APP_ENV = "build"
