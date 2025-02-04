038c69fea9cde4ef9e44c8bd8f1fe50ddb7c9ed5
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2025-02-04 14:58:38 +0000

    Simplified context for both post and get methods

--- src/interactions/views.py
+++ src/interactions/views.py
@@ -61,47 +61,43 @@ def react_to_page(request, *args, pk, **kwargs):
     page = get_object_or_404(Page, id=pk)
     user = request.user
 
-    if request.method == "POST":
-        reaction_type = ReactionType(request.POST["reaction_type"])
-        is_selected = request.POST.get("is_selected") == "true"
-        if is_selected:
-            reactions_service.react_to_page(user, page, None)
-        else:
-            reactions_service.react_to_page(user, page, reaction_type)
-
-        context = {
-            "csrf_token": request.META.get("CSRF_COOKIE", ""),
-            "request": request,
-            "post_url": reverse(viewname="interactions:reactions", kwargs={"pk": pk}),
-            "user_reaction": reaction_type if not is_selected else None,
-            "reaction_type": reaction_type,
-            "reaction_count": reactions_service.get_reaction_count(
-                page=page, reaction_type=reaction_type
-            ),
-        }
-
-        return TemplateResponse(
-            request=request,
-            template="interactions/reaction_button.html",
-            context=context,
-            headers={"HX-Trigger": "refresh-reactions"},
-        )
-
-    reactions = reactions_service.get_reaction_counts(page)
-    user_reaction = reactions_service.get_user_reaction(user, page)
-
     context = {
         "get_url": reverse("interactions:reactions", kwargs={"pk": pk}),
         "post_url": reverse("interactions:reactions", kwargs={"pk": pk}),
         "csrf_token": request.META.get("CSRF_COOKIE", ""),
         "page": page,
         "request": request,
-        "user_reaction": user_reaction,
-        "reactions": reactions,
     }
 
+    if request.method == "POST":
+        reaction_type = ReactionType(request.POST["reaction_type"])
+        is_selected = request.POST.get("is_selected") == "true"
+
+        reacted_type = None if is_selected else reaction_type
+        reactions_service.react_to_page(user, page, reacted_type)
+
+        context.update(
+            {
+                "user_reaction": reaction_type if not is_selected else None,
+                "reaction_type": reaction_type,
+                "reaction_count": reactions_service.get_reaction_count(
+                    page=page, reaction_type=reaction_type
+                ),
+            }
+        )
+        template = "interactions/reaction_button.html"
+        headers = {"HX-Trigger": "refresh-reactions"}
+
+    elif request.method == "GET":
+        context.update(
+            {
+                "reactions": reactions_service.get_reaction_counts(page),
+                "user_reaction": reactions_service.get_user_reaction(user, page),
+            }
+        )
+        template = ("interactions/reactions.html",)
+        headers = {}
+
     return TemplateResponse(
-        request=request,
-        template="interactions/reactions.html",
-        context=context,
+        request=request, template=template, context=context, headers=headers
     )
