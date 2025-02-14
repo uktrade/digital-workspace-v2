26a270fd0ef698b56beddc5440f5c36442f39e0c
David Okeke <david.okeke@madetech.com>
2025-02-14 10:29:31 +0000

    removed page object and passed flags directly to avoid confusion

--- src/home/templatetags/cards.py
+++ src/home/templatetags/cards.py
@@ -30,7 +30,8 @@ def page_to_display_context(page: NewsPage | EventPage, hide_updated: bool = Fal
             created_date=page.first_published_at,
             comment_count=page.comment_count,
             reaction_count=page.reaction_count,
-            page=page,
+            allow_comments=page.allow_comments,
+            allow_reactions=page.allow_reactions,
         )
         if not hide_updated:
             context.update(
