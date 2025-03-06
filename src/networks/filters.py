d77e5ec5eee5509b2bc4f690e508637513e30dcc
David Okeke <david.okeke@madetech.com>
2025-03-06 15:01:18 +0000

    Updated network type filter

--- src/networks/filters.py
+++ src/networks/filters.py
@@ -5,9 +5,22 @@ from networks.models import Network
 
 
 class NetworksFilters(django_filters.FilterSet):
+
+    def get_network_type_choices():
+        network_types = (
+            Network.objects.exclude(network_type__isnull=True)
+            .exclude(network_type="")
+            .values_list("network_type", flat=True)
+            .distinct()
+        )
+        return [
+            (network_type, Network.NetworkTypes(network_type).label)
+            for network_type in network_types
+        ]
+
     network_type = django_filters.ChoiceFilter(
         field_name="network_type",
-        choices=Network.NetworkTypes,
+        choices=get_network_type_choices(),
         lookup_expr="exact",
         widget=forms.widgets.Select(attrs={"class": "dwds-select"}),
         empty_label="All network types",
