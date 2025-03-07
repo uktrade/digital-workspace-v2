8c9321248a8942f2636e4f9e59f08e6be6c7c6e1
David Okeke <140066072+david-okeke1337@users.noreply.github.com>
2025-03-07 13:23:41 +0000

    INTR-608 - Only show network types in Network type filter if they have results (#958)

--- src/networks/filters.py
+++ src/networks/filters.py
@@ -5,9 +5,23 @@ from networks.models import Network
 
 
 class NetworksFilters(django_filters.FilterSet):
+
+    def get_network_type_choices():
+        network_types = (
+            Network.objects.live()
+            .public()
+            .exclude(network_type__isnull=True)
+            .values_list("network_type", flat=True)
+            .distinct()
+        )
+        return [
+            (network_type, Network.NetworkTypes(network_type).label)
+            for network_type in network_types
+        ]
+
     network_type = django_filters.ChoiceFilter(
         field_name="network_type",
-        choices=Network.NetworkTypes,
+        choices=get_network_type_choices(),
         lookup_expr="exact",
         widget=forms.widgets.Select(attrs={"class": "dwds-select"}),
         empty_label="All network types",
