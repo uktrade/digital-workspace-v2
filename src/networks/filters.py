bef55f618a276739f065da7db113b9ff879d56a8
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-03-10 14:28:09 +0000

    Fix the network type choices functionality

--- src/networks/filters.py
+++ src/networks/filters.py
@@ -2,26 +2,13 @@ import django_filters
 from django import forms
 
 from networks.models import Network
+from networks.utils import get_active_network_types
 
 
 class NetworksFilters(django_filters.FilterSet):
-
-    def get_network_type_choices():
-        network_types = (
-            Network.objects.live()
-            .public()
-            .exclude(network_type__isnull=True)
-            .values_list("network_type", flat=True)
-            .distinct()
-        )
-        return [
-            (network_type, Network.NetworkTypes(network_type).label)
-            for network_type in network_types
-        ]
-
     network_type = django_filters.ChoiceFilter(
         field_name="network_type",
-        choices=get_network_type_choices(),
+        choices=get_active_network_types,
         lookup_expr="exact",
         widget=forms.widgets.Select(attrs={"class": "dwds-select"}),
         empty_label="All network types",
