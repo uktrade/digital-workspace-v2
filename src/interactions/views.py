270dae1d62985dbf0327942d4be3c2fccf7f8947
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2025-03-11 09:39:31 +0000

    Implemented endpoint for comments's reactions

--- src/interactions/views.py
+++ src/interactions/views.py
@@ -7,8 +7,10 @@ from wagtail.models import Page
 
 from interactions.models import ReactionType
 from interactions.services import bookmarks as bookmarks_service
+from interactions.services import comment_reactions as comment_reactions_service
 from interactions.services import comments as comments_service
 from interactions.services import page_reactions as page_reactions_service
+from news.models import Comment
 
 
 @require_http_methods(["POST"])
@@ -70,6 +72,28 @@ def react_to_page(request, *args, pk, **kwargs):
         {
             "user_reaction": page_reactions_service.get_user_page_reaction(user, page),
             "reactions": page_reactions_service.get_page_reaction_counts(page),
+        }
+    )
+
+
+@require_http_methods(["POST"])
+def react_to_comment(request, *args, pk, **kwargs):
+    comment = get_object_or_404(Comment, id=pk)
+    user = request.user
+
+    if request.method == "POST":
+        reaction_type = ReactionType(request.POST["reaction_type"])
+        is_selected = request.POST.get("is_selected") == "true"
+
+        reacted_type = None if is_selected else reaction_type
+        comment_reactions_service.react_to_comment(user, comment, reacted_type)
+
+    return JsonResponse(
+        {
+            "user_reaction": comment_reactions_service.get_user_comment_reaction(
+                user, comment
+            ),
+            "reactions": comment_reactions_service.get_comment_reaction_counts(comment),
         }
     )
 
