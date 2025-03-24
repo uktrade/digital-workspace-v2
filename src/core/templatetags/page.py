66227b2994cf27c17bb7228e6e5e30a98dd4613e
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-03-24 13:00:33 +0000

    Add template tag approach

--- src/core/templatetags/page.py
+++ src/core/templatetags/page.py
@@ -1,4 +1,7 @@
+import urllib.parse
+
 from django import template
+from django.http import HttpRequest
 from django.urls import reverse
 from wagtail.models import Page
 
@@ -16,3 +19,27 @@ def page_author(page: Page):
         "published_timestamp": page.first_published_at,
         "updated_timestamp": page.last_published_at,
     }
+
+
+@register.simple_tag
+def get_share_page_email_link(request: HttpRequest, page: Page) -> str:
+    to_email = ""
+    params: dict[str, str] = {
+        "cc": "",
+        "bcc": "",
+        "subject": f"{page.title} - DBT Intranet",
+        "body": (
+            page.get_full_url(request)
+            + "\n"
+            + "Only people with access to the Department for Business and Trade"
+            " intranet can open this link."
+        ),
+    }
+    params_str = "&".join(
+        [
+            f"{key}={urllib.parse.quote_plus(value)}"
+            for key, value in params.items()
+            if value
+        ]
+    )
+    return f"mailto:{to_email}?{params_str}"
