dd41bf1d52b781cafca8dfb3e3a9678355e4f197
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-03-24 14:32:00 +0000

    Simple perm sec as author change

--- src/core/templatetags/page.py
+++ src/core/templatetags/page.py
@@ -1,6 +1,9 @@
 from django import template
+from django.conf import settings
 from django.urls import reverse
 from wagtail.models import Page
+
+from peoplefinder.models import Person
 
 
 register = template.Library()
@@ -9,6 +12,13 @@ register = template.Library()
 @register.inclusion_tag("dwds/components/author.html")
 def page_author(page: Page):
     person = page.owner.profile
+
+    if getattr(page, "perm_sec_as_author", False) and settings.PERM_SEC_NAME:
+        perm_sec_names = settings.PERM_SEC_NAME.split(" ")
+        person = Person.objects.get(
+            first_name=perm_sec_names[0], last_name=perm_sec_names[1]
+        )
+
     return {
         "name": person.full_name,
         "profile_image_url": (person.photo.url if person.photo else None),
