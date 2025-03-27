813a88ae11e96a2e91d9c5f8feca675caf8d5159
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-03-27 15:53:03 +0000

    Add bookmark e2e tests

--- src/e2e_tests/pages/homepage.py
+++ src/e2e_tests/pages/homepage.py
@@ -1,4 +1,7 @@
 from .base import SitePage
 
 
-class HomePage(SitePage): ...
+class HomePage(SitePage):
+    @property
+    def bookmarks(self):
+        return self.page.get_by_test_id("bookmark-link").all()
