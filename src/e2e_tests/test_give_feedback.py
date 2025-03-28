ab526a28e6f594500b7f7cf680d37e024c6b1764
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-03-28 11:56:57 +0000

    Add e2e test for the give feedback button

---
+++ src/e2e_tests/test_give_feedback.py
@@ -0,0 +1,25 @@
+import re
+
+import pytest
+from playwright.sync_api import Page, expect
+
+from news.factories import NewsPageFactory
+from news.models import NewsHome
+
+from .pages.homepage import HomePage
+from .utils import login
+
+
+@pytest.mark.e2e
+def test_give_feedback(superuser, user, page: Page):
+    news_home_page = NewsHome.objects.get()
+    news_page = NewsPageFactory.create(parent=news_home_page)
+
+    login(page, user)
+
+    # Go to the news page and click the "Give feedback" button
+    page.goto(news_page.get_url())
+    page.get_by_test_id("give-feedback-button").click()
+
+    # Check to see that the browser's focus is the feedback form
+    expect(page.locator("textarea[name='trying_to']")).to_be_focused()
