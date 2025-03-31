17bab2d3fa04affc2b41f9c7bda23c2b4df3b001
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-03-31 14:27:11 +0100

    INTR-604 Perm sec as author updates (#981)

--- src/core/templatetags/page.py
+++ src/core/templatetags/page.py
@@ -11,14 +11,32 @@ register = template.Library()
 
 @register.inclusion_tag("dwds/components/author.html")
 def page_author(page: Page):
-    person = page.owner.profile
-    return {
-        "name": person.full_name,
-        "profile_image_url": (person.photo.url if person.photo else None),
-        "profile_url": reverse("profile-view", args=[person.slug]),
+    page = page.specific
+
+    context = {
+        "name": "",
+        "profile_image_url": None,
+        "profile_url": None,
         "published_timestamp": page.first_published_at,
         "updated_timestamp": page.last_published_at,
     }
+
+    author = page.get_author()
+
+    if isinstance(author, str):
+        context["name"] = author
+        return context
+
+    context.update(
+        name=author.full_name,
+        profile_image_url=(author.photo.url if author.photo else None),
+    )
+    if author.is_active:
+        context.update(
+            profile_url=reverse("profile-view", args=[author.slug]),
+        )
+
+    return context
 
 
 @register.simple_tag
