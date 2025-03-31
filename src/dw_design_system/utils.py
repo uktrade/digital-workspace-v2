0050479ea401edb66ac7a6e6e4b49b354107513f
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2025-03-31 13:46:27 +0100

    INTR-618: Implemented quote component (#983)
    Co-authored-by: Cameron Lamb <cameron.lamb@digital.trade.gov.uk>

--- src/dw_design_system/utils.py
+++ src/dw_design_system/utils.py
@@ -166,6 +166,20 @@ def get_dwds_templates(template_type, request: HttpRequest):
                 },
             },
             {
+                "name": "Quotes Component",
+                "template": "dwds/components/quote.html",
+                "context": {
+                    "highlight": True,
+                    "quote": "It makes you happy that you provided several kilowatts to the power sector, and it means that someone will turn their lights on today. That motivates me, that’s your work really impacts the wellbeing of the country.",
+                    "source_name": "Pavolo Sorokin",
+                    "source_url": "https://www.gov.uk",
+                    "source_image": thumbnail_url,
+                    "source_role": "Trade policy and Healthcare Adviser",
+                    "source_team": "Ukraine",
+                    "source_team_url": "https://www.gov.uk",
+                },
+            },
+            {
                 "name": "Spotlight",
                 "template": "dwds/components/spotlight.html",
                 "context": {
