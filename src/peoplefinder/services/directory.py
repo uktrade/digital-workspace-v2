68d074944e9b5f93bc277e902845f0a4a7b5a7e4
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-07 17:39:00 +0100

    add comment

--- src/peoplefinder/services/directory.py
+++ src/peoplefinder/services/directory.py
@@ -6,7 +6,11 @@ from user.models import User
 
 
 def get_people(user: User) -> QuerySet[Person]:
+    """
+    Return all the people that the given user has permission to see
+    """
     if user.has_perm("peoplefinder.can_view_inactive_profiles"):
         return Person.objects.all()
+    
     days = settings.SEARCH_SHOW_INACTIVE_PROFILES_WITHIN_DAYS
     return Person.objects.active_or_inactive_within(days=days)
