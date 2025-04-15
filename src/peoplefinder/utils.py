a1bc4ddcf3d505f236a9c241fcc50a49e8603e7f
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-15 17:48:04 +0100

    formatting fix

--- src/peoplefinder/utils.py
+++ src/peoplefinder/utils.py
@@ -2,7 +2,11 @@ from peoplefinder.models import UkStaffLocation
 
 
 def get_uk_locations() -> list[tuple[str]]:
-    cities = UkStaffLocation.objects.order_by("city").values_list("city", flat=True).distinct()
+    cities = (
+        UkStaffLocation.objects.order_by("city")
+        .values_list("city", flat=True)
+        .distinct()
+    )
     return [(city, city) for city in cities]
 
 
