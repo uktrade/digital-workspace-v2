a98296991581b5294a1d665a16a7c46159da4f5c
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-15 17:19:29 +0100

    do we want the building filter

--- src/peoplefinder/filters.py
+++ src/peoplefinder/filters.py
@@ -1,17 +1,17 @@
 import django_filters
 from django import forms
 
-from peoplefinder.utils import get_uk_buildings, get_uk_locations
+from peoplefinder.utils import get_uk_locations
 
 
 class DiscoverFilters(django_filters.FilterSet):
-    building_name = django_filters.ChoiceFilter(
-        field_name="uk_office_location__building_name",
-        choices=get_uk_buildings,
-        widget=forms.widgets.Select(
-            attrs={"class": "dwds-select", "style": "padding-right: 0"}
-        ),
-    )
+    # building_name = django_filters.ChoiceFilter(
+    #     field_name="uk_office_location__building_name",
+    #     choices=get_uk_buildings,
+    #     widget=forms.widgets.Select(
+    #         attrs={"class": "dwds-select", "style": "padding-right: 0"}
+    #     ),
+    # )
 
     city = django_filters.ChoiceFilter(
         field_name="uk_office_location__city",
