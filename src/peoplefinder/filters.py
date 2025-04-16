a7d85b2da7f15cc4a4dc1f10004f1282ce77a73c
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-16 10:22:28 +0100

    removed building filter from this pr

--- src/peoplefinder/filters.py
+++ src/peoplefinder/filters.py
@@ -1,20 +1,12 @@
 import django_filters
 from django import forms
 
-from peoplefinder.utils import get_uk_locations
+from peoplefinder.utils import get_uk_city_locations
 
 
 class DiscoverFilters(django_filters.FilterSet):
-    # building_name = django_filters.ChoiceFilter(
-    #     field_name="uk_office_location__building_name",
-    #     choices=get_uk_buildings,
-    #     widget=forms.widgets.Select(
-    #         attrs={"class": "dwds-select", "style": "padding-right: 0"}
-    #     ),
-    # )
-
     city = django_filters.ChoiceFilter(
         field_name="uk_office_location__city",
-        choices=get_uk_locations,
+        choices=get_uk_city_locations,
         widget=forms.widgets.Select(attrs={"class": "dwds-select"}),
     )
--- src/peoplefinder/utils.py
+++ src/peoplefinder/utils.py
@@ -1,17 +1,10 @@
 from peoplefinder.models import UkStaffLocation
 
 
-def get_uk_locations() -> list[tuple[str]]:
+def get_uk_city_locations() -> list[tuple[str]]:
     cities = (
         UkStaffLocation.objects.order_by("city")
         .values_list("city", flat=True)
         .distinct()
     )
     return [(city, city) for city in cities]
-
-
-def get_uk_buildings() -> list[tuple[str]]:
-    buildings = UkStaffLocation.objects.values_list(
-        "building_name", flat=True
-    ).distinct()
-    return [(building, building) for building in buildings]
