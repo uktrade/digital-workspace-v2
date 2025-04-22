dcd9e4d2b9d352d866331c77dcc7d9d6106b097a
David Okeke <david.okeke@madetech.com>
2025-04-22 14:05:05 +0100

    fix countries for test_page_problem_found_view

---
+++ src/core/test/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+from django.core.management import call_command
+
+
+@pytest.fixture(scope="package")
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command("create_section_homepages")
+        call_command("loaddata", "countries.json")
