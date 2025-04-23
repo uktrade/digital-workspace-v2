08d8f86ad14b0b30dbe3aea9f53911f1eb643f32
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-23 15:47:00 +0100

    simplify filter

--- src/peoplefinder/filters.py
+++ src/peoplefinder/filters.py
@@ -4,22 +4,12 @@ from django import forms
 from peoplefinder.utils import get_uk_city_locations
 
 
-ORDER_CHOICES = [("ascending", "Ascending"), ("descending", "Descending")]
-
-
 class DiscoverFilters(django_filters.FilterSet):
     city = django_filters.ChoiceFilter(
         field_name="uk_office_location__city",
         choices=get_uk_city_locations,
         widget=forms.widgets.Select(attrs={"class": "dwds-select"}),
     )
-    sort_by_first_name = django_filters.ChoiceFilter(
-        field_name="first_name",
-        label="sort_by_first_name",
-        choices=ORDER_CHOICES,
-        method="filter_by_order",
+    sort_by = django_filters.OrderingFilter(
+        fields=(("first_name", "first_name"),),
     )
-
-    def filter_by_order(self, queryset, field_name, value):
-        expression = field_name if value == "ascending" else f"-{field_name}"
-        return queryset.order_by(expression)
