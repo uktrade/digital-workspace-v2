b2d22d78b0e0101ce84e687f373a791809464137
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-24 17:05:40 +0100

    IDPF-397 Build seniority, add grade sorting option (#1040)

--- src/peoplefinder/filters.py
+++ src/peoplefinder/filters.py
@@ -1,8 +1,26 @@
 import django_filters
 from django import forms
+from django.db.models import QuerySet
 
 from core.filters import FilterSet
+from peoplefinder.models import Person
 from peoplefinder.utils import get_uk_city_locations
+
+
+ORDER_CHOICES = {
+    "grade": {
+        "label": "Grade",
+        "ordering": ("grade", "first_name", "last_name"),
+    },
+    "first_name": {
+        "label": "First name",
+        "ordering": ("first_name", "last_name"),
+    },
+    "last_name": {
+        "label": "Last name",
+        "ordering": ("last_name", "first_name"),
+    },
+}
 
 
 class DiscoverFilters(FilterSet):
@@ -12,5 +30,13 @@ class DiscoverFilters(FilterSet):
         widget=forms.widgets.Select(attrs={"class": "dwds-select"}),
     )
     sort_by = django_filters.OrderingFilter(
-        fields=(("first_name", "first_name"),),
+        choices=[
+            (choice_key, choice_value["label"])
+            for choice_key, choice_value in ORDER_CHOICES.items()
+        ],
+        method="apply_ordering",
     )
+
+    def apply_ordering(self, queryset, name, value) -> QuerySet[Person]:
+        order_fields = ORDER_CHOICES[value[0]]["ordering"]
+        return queryset.order_by(*order_fields)
