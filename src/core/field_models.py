d887bf12445cd8af1a7481211899c61104a065c5
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-04-25 12:14:16 +0100

    Only strip tags if the field doesn't use choices

--- src/core/field_models.py
+++ src/core/field_models.py
@@ -4,7 +4,12 @@ from django.utils.html import strip_tags
 
 class CharField(models.CharField):
     def clean(self, value, model_instance):
-        return strip_tags(super().clean(value, model_instance))
+        cleaned_value = super().clean(value, model_instance)
+
+        if not self.choices:
+            return strip_tags(cleaned_value)
+
+        return cleaned_value
 
 
 # JSONField Need to test rich text and plain text in streamfields
@@ -12,4 +17,9 @@ class CharField(models.CharField):
 
 class TextField(models.TextField):
     def clean(self, value, model_instance):
-        return strip_tags(super().clean(value, model_instance))
+        cleaned_value = super().clean(value, model_instance)
+
+        if not self.choices:
+            return strip_tags(cleaned_value)
+
+        return cleaned_value
