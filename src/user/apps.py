6d021ec0606c806e06f0efd3d09aac5029f156e3
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-04-25 12:20:58 +0100

    Don't run this logic for the user app

--- src/user/apps.py
+++ src/user/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     name = "user"
     verbose_name = "User"
-
-    def ready(self):
-        from core.signals import add_validators
-
-        add_validators(self)
