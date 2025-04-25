eb6fc4d5bbca4fed8b23f9099d1449d83142f38b
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-04-25 17:00:26 +0100

    update fn name reference data

--- src/peoplefinder/services/reference.py
+++ src/peoplefinder/services/reference.py
@@ -1,4 +1,4 @@
-from core.utils import get_data
+from core.utils import get_data_for_django_filters
 from peoplefinder.models import (
     AdditionalRole,
     Grade,
@@ -13,39 +13,41 @@ from peoplefinder.utils import cache_for_one_hour
 
 @cache_for_one_hour
 def get_uk_city_locations() -> list[tuple[str, str]]:
-    return get_data(model=UkStaffLocation, field_name="city")
+    return get_data_for_django_filters(model=UkStaffLocation, field_name="city")
 
 
 @cache_for_one_hour
 def get_uk_buildings() -> list[tuple[str, str]]:
-    return get_data(model=UkStaffLocation, field_name="building_name")
+    return get_data_for_django_filters(
+        model=UkStaffLocation, field_name="building_name"
+    )
 
 
 @cache_for_one_hour
 def get_grades() -> list[tuple[str, str]]:
-    return get_data(model=Grade, field_name="name")
+    return get_data_for_django_filters(model=Grade, field_name="name")
 
 
 @cache_for_one_hour
 def get_professions() -> list[tuple[str, str]]:
-    return get_data(model=Profession, field_name="name")
+    return get_data_for_django_filters(model=Profession, field_name="name")
 
 
 @cache_for_one_hour
 def get_key_skills() -> list[tuple[str, str]]:
-    return get_data(model=KeySkill, field_name="name")
+    return get_data_for_django_filters(model=KeySkill, field_name="name")
 
 
 @cache_for_one_hour
 def get_learning_interests() -> list[tuple[str, str]]:
-    return get_data(model=LearningInterest, field_name="name")
+    return get_data_for_django_filters(model=LearningInterest, field_name="name")
 
 
 @cache_for_one_hour
 def get_networks() -> list[tuple[str, str]]:
-    return get_data(model=Network, field_name="name")
+    return get_data_for_django_filters(model=Network, field_name="name")
 
 
 @cache_for_one_hour
 def get_additional_roles() -> list[tuple[str, str]]:
-    return get_data(model=AdditionalRole, field_name="name")
+    return get_data_for_django_filters(model=AdditionalRole, field_name="name")
