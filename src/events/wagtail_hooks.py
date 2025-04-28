d8852b3c0471c898f4839060aaf648a9baee6e94
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-04-28 14:56:28 +0100

    Run `make fix`

--- src/events/wagtail_hooks.py
+++ src/events/wagtail_hooks.py
@@ -6,6 +6,4 @@ from wagtail import hooks
 @hooks.register("insert_editor_js")
 def editor_js():
     events_admin_js_path = static("admin/events.js")
-    return mark_safe(  # noqa: S308
-        f"<script src='{events_admin_js_path}'></script>"
-    )
+    return mark_safe(f"<script src='{events_admin_js_path}'></script>")  # noqa: S308
