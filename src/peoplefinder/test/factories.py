1b98c9bcf25b05ba7b3e44e3a2a209700127d2e7
Alsen Aligullu <alsen.aligullu@digital.trade.gov.uk>
2025-05-01 11:08:56 +0100

    remove PersonFactory attributes

--- src/peoplefinder/test/factories.py
+++ src/peoplefinder/test/factories.py
@@ -1,24 +1,16 @@
+import uuid
 import factory
-import factory.fuzzy
 
-from peoplefinder.models import Person, Team, UkStaffLocation
-from user.models import User
+from peoplefinder.models import (
+    Person,
+    Team,
+)
 from user.test.factories import UserFactory
-from django.core.management import call_command
 
 
 class PersonFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Person
-
-    legacy_sso_user_id = factory.Sequence(lambda n: f"legacy_sso_{n}")
-
-    is_active = True
-    user = factory.SubFactory(UserFactory)
-    uk_office_location = factory.Iterator(UkStaffLocation.objects.all())
-    first_name = factory.fuzzy.FuzzyText(length=12)
-    preferred_first_name = factory.fuzzy.FuzzyText(length=12)
-    last_name = factory.fuzzy.FuzzyText(length=12)
 
 
 class UserWithPersonFactory(UserFactory):
