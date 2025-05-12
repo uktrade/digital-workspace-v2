f5a2cbbb60b62a043d64b650f1449b50a468db62
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-05-12 12:34:48 +0100

    Commit fixed person_subscriptions service

--- src/interactions/services/person_subscriptions.py
+++ src/interactions/services/person_subscriptions.py
@@ -7,17 +7,17 @@ from user.models import User
 
 def subscribe(*, person: Person, user: User) -> PersonSubscription:
     team_subscription, _ = PersonSubscription.objects.get_or_create(
-        user=user, team=team
+        user=user, person=person
     )
     return team_subscription
 
 
 def unsubscribe(*, person: Person, user: User) -> None:
-    PersonSubscription.objects.filter(user=user, team=team).delete()
+    PersonSubscription.objects.filter(user=user, person=person).delete()
 
 
 def is_subscribed(*, person: Person, user: User) -> bool:
-    return PersonSubscription.objects.filter(user=user, team=team).exists()
+    return PersonSubscription.objects.filter(user=user, person=person).exists()
 
 
 def get_subscribed_people(*, user: User) -> QuerySet[Person]:
