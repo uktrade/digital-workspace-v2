450a1625659703ab2c765bea7372a6d2be6cfa70
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-05-12 16:44:00 +0100

    INTR-689 Remove old network flags (#1069)

--- src/core/flags.py
+++ src/core/flags.py
@@ -1,6 +1,4 @@
 NEW_GTM_DATALAYER = "new_gtm_datalayer"
-NETWORK_TYPES = "network_types"
-NETWORKS_HUB = "networks_hub"
 SHARE_TEAMS = "share_teams"
 NEW_COMMENTS = "new_comments"
 NEW_NEWS_AUTHOR = "new_news_author"
