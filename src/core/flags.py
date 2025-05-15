667832f4cdf00fd98ce8af4f74a23d0988c1a9b8
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-05-15 09:21:50 +0100

    INTR-688 remove `new_comments` flag (#1068)
    Co-authored-by: Nico Picchio <nico.picchio@digital.trade.gov.uk>

--- src/core/flags.py
+++ src/core/flags.py
@@ -1,6 +1,5 @@
 NEW_GTM_DATALAYER = "new_gtm_datalayer"
 SHARE_TEAMS = "share_teams"
-NEW_COMMENTS = "new_comments"
 NEW_NEWS_AUTHOR = "new_news_author"
 EXTERNAL_LINKS = "external_links"
 PF_DISCOVER = "pf_discover"
