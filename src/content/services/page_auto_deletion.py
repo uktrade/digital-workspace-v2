9eb52737f85faa23260bc3c9f05189d6d54f3806
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2025-05-16 12:30:25 +0100

    adds function to get all pages that need archiving

--- src/content/services/page_auto_deletion.py
+++ src/content/services/page_auto_deletion.py
@@ -11,6 +11,7 @@ from home.models import HomePage
 from networks.models import NetworksHome
 from news.models import NewsHome
 from tools.models import Tool, ToolsHome
+from user.models import User
 from working_at_dit.models import (
     GuidanceHome,
     HowDoIHome,
@@ -43,20 +44,32 @@ PAGES_TO_EXCLUDE = [
     WorkingAtDITHome,
 ]
 
-CUTOFF_DAYS = 365
+
+CUTOFF = now() - timedelta(days=365)
 
 
 # ask if this function is fine or if we want to pass a cutoff date a parameter to make it more flexible
 # eg cutoff_date=None -> if cutoff_date is None: cutoff_date = now() - timedelta(days=365)
 def get_pages_to_update() -> QuerySet[BasePage]:
-    a_year_ago = now() - timedelta(days=CUTOFF_DAYS)
     return BasePage.objects.not_exact_type(*PAGES_TO_EXCLUDE).filter(
-        Q(confirmation_page_needed_at__lte=a_year_ago)
+        Q(confirmation_page_needed_at__lte=CUTOFF)
         | Q(confirmation_page_needed_at__isnull=True),
-        last_published_at__lte=a_year_ago,
+        last_published_at__lte=CUTOFF,
     )
 
 
 def confirm_page_still_needed(page: BasePage) -> None:
     page.confirmation_page_needed_at = now()
     page.save(update_fields=["confirmation_page_needed_at"])
+
+
+def get_pages_to_archive() -> QuerySet[BasePage]:
+    return BasePage.objects.not_exact_type(*PAGES_TO_EXCLUDE).filter(
+        Q(confirmation_page_needed_at_isnull=True)
+        | Q(confirmation_page_needed_at__lte=CUTOFF),
+        page_update_notified_at=now() - timedelta(days=30),
+        last_published_at__lte=CUTOFF,
+    )
+
+
+def get_users_to_contact(page: BasePage) -> QuerySet[User]: ...
