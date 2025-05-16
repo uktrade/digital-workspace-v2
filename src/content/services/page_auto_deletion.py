e2101829deb2916b8de9e22b3dbc3322e86e8894
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2025-05-16 11:31:25 +0100

    adds function to confirm a page is still needed

--- src/content/services/page_auto_deletion.py
+++ src/content/services/page_auto_deletion.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from django.db.models import Q
+from django.db.models import Q, QuerySet
 from django.utils.timezone import now
 
 from about_us.models import AboutUsHome
@@ -45,12 +45,18 @@ PAGES_TO_EXCLUDE = [
 
 CUTOFF_DAYS = 365
 
+
 # ask if this function is fine or if we want to pass a cutoff date a parameter to make it more flexible
 # eg cutoff_date=None -> if cutoff_date is None: cutoff_date = now() - timedelta(days=365)
-def get_pages_to_update():
+def get_pages_to_update() -> QuerySet[BasePage]:
     a_year_ago = now() - timedelta(days=CUTOFF_DAYS)
     return BasePage.objects.not_exact_type(*PAGES_TO_EXCLUDE).filter(
         Q(confirmation_page_needed_at__lte=a_year_ago)
         | Q(confirmation_page_needed_at__isnull=True),
         last_published_at__lte=a_year_ago,
     )
+
+
+def confirm_page_still_needed(page: BasePage) -> None:
+    page.confirmation_page_needed_at = now()
+    page.save(update_fields=["confirmation_page_needed_at"])
