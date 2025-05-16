3f9244bd1f8f7e481bfc7bcfa7b9f573b3a0ddb1
Nico Picchio <nico.picchio@digital.trade.gov.uk>
2025-05-16 15:03:05 +0100

    adds method to get latest publisher

--- src/content/models.py
+++ src/content/models.py
@@ -363,6 +363,14 @@ class BasePage(Page, Indexed):
 
         return None
 
+    def get_latest_publisher(self) -> Optional[UserModel]:
+        """Return the latest publisher of the page or None."""
+        last_revision_with_user = (
+            self.revisions.exclude(user=None).latest("created_at")
+        )
+
+        return last_revision_with_user.user if last_revision_with_user else None
+
     @property
     def days_since_last_published(self):
         if self.last_published_at:
