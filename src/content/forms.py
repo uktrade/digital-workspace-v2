3d76e6e8711ac3be9ba3a8fe724d0e6b60ab803b
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2025-05-23 16:34:17 +0100

    Set the initial queryset to `TeamMember.objects.none()`

--- src/content/forms.py
+++ src/content/forms.py
@@ -1,5 +1,6 @@
 from wagtail.admin.forms import WagtailAdminPageForm
 
+from peoplefinder.models import TeamMember
 from peoplefinder.services.person import get_roles
 
 
@@ -7,5 +8,6 @@ class BasePageForm(WagtailAdminPageForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.fields["page_author_role"].queryset = TeamMember.objects.none()
         if page_author := self.data.get("page_author"):
             self.fields["page_author_role"].queryset = get_roles(person_pk=page_author)
