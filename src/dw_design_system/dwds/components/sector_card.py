1eaac09ab1e93b207b5ae84ddc901be1f6b9a4b9
David Okeke <140066072+david-okeke1337@users.noreply.github.com>
2025-05-27 18:43:17 +0100

    INTR-681 - HR VR landing page (#1101)

---
+++ src/dw_design_system/dwds/components/sector_card.py
@@ -0,0 +1,35 @@
+from wagtail import blocks
+
+import dw_design_system.dwds.components as dwds_blocks
+from content import blocks as content_blocks
+
+
+class SectorCardBlock(blocks.StructBlock):
+    """Sector section"""
+
+    sector_title = content_blocks.HeadingBlock(required=False, max_length=30)
+    sector_elements = blocks.StreamBlock(
+        [
+            ("dw_navigation_card", dwds_blocks.NavigationCardBlock()),
+        ],
+        required=True,
+        label="Sector Elements",
+    )
+
+    class Meta:
+        template = "dwds/components/sector_card.html"
+        icon = "form"
+        label = "Sector with elements"
+
+    def get_context(self, value, parent_context=None):
+        context = parent_context or {}
+
+        context.update(
+            sector_title=value["sector_title"],
+            sector_elements=value["sector_elements"],
+        )
+        return context
+
+    def get_searchable_heading(self, value):
+        title = value["sector_title"]
+        return title
