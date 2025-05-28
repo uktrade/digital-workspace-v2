8a528315f2983c90fd8f6aa9e458839d6a204c66
marcelkornblum <me@marcelkornblum.com>
2025-05-28 18:02:05 +0100

    sections implemented roughly

--- src/core/context_processors.py
+++ src/core/context_processors.py
@@ -4,7 +4,13 @@ from core.utils import get_all_feature_flags, get_external_link_settings
 
 
 def global_context(request):
+    section = "main"
+    if "working-at-dbt" in request.path:
+        section = "working_here"
+    if "peoplefinder" in request.path:
+        section = "people"
     return {
+        "SECTION": section,
         "USER_IS_AUTHENTICATED": request.user.is_authenticated,
         "PAGE_PROBLEM_FORM": PageProblemFoundForm(
             initial={"page_url": request.build_absolute_uri()}
